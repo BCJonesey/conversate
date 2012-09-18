3013ebd52771cbd1f995b7fb09649324211fd5c1
Nick Martin <martin.nick@gmail.com>
2012-09-17 22:14:53 -0400

    Start of users.

--- conversate.js
+++ conversate.js
@@ -7,11 +7,22 @@ if (Meteor.is_client) {
 
   Template.editor.events = {
 	'click input#send': function () {
-		Messages.insert({subject: $('#subject').val(), body: $('#body').val()});
+		var name = Session.get('name');
+		if (!name)
+		{
+			name = 'anonymous coward'
+		}
+		Messages.insert({subject: $('#subject').val(), body: $('#body').val(), from: name});
 	},
 	'click input#nuke': function () {
 		Messages.remove({});
 	}
+  };
+
+  Template.users.events = {
+    'click input#be': function () {
+		Session.set('name', $('#name').val());
+    }	
   };
 }
 
@@ -19,7 +30,7 @@ if (Meteor.is_server) {
   Meteor.startup(function () {
     if (Messages.find().count() === 0)
 	{
-		Messages.insert({subject: "Message one", body: "Some random body text."});
+		Messages.insert({subject: "Message one", body: "Some random body text.", from: "anonymous coward"});
 	}
   });
 }
