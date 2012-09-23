5cfe6d54d3722d012e2df56522644a743af5593a
Nick Martin <martin.nick@gmail.com>
2012-09-23 11:57:41 -0400

    Ugly signin/signout.

--- conversate.js
+++ conversate.js
@@ -13,8 +13,25 @@ if (Meteor.is_client) {
 		if(event.keyCode == 13) {
 			send_message();
 		}
+	}	
+ 	};
+
+	Template.page.helpers({
+	'has_user': function () {
+		var user = Session.get('name');
+		if (user)
+		{
+			return true;
+		}
+		return false;
 	}
-  };
+	});
+
+	Template.nav.events({
+	'click .nav-signout': function () {
+		Session.set('name', null);
+	}
+	});
 
 }
 
