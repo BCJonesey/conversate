7ff1c088c35bac4c766809cfc58a53251d6e9ded
Nick Martin <martin.nick@gmail.com>
2012-09-30 14:03:15 -0400

    Added basic rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conversate::Application.config.secret_token = '605cfe34ca93325aeac1c938a52aec64aa1c35169dbcd81921d574226d33e52dc14f82538a1c83ec6b084fdced96e72fa6d39cde19afc7409753137c87cfa545'
