e8757c52a20dc922cc8cd3715a8f1c1237eb2d78
Sean Kermes <skermes@gmail.com>
2012-11-27 15:02:17 -0500

    Creating empty conversation model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127174927) do
+ActiveRecord::Schema.define(:version => 20121127200108) do
+
+  create_table "conversations", :force => true do |t|
+    t.string   "subject"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                        :null => false
