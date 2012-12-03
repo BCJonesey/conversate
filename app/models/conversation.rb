24b42aca78503e575228a6d263fe1fa07d3be422
Sean Kermes <skermes@gmail.com>
2012-12-02 23:12:22 -0500

    Documenting Conversation.pieces method (to come).

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,19 @@
 class Conversation < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :events, :inverse_of => :conversation
+
+  # Public: Stitch together the events on a conversation into "conversation
+  # pieces" that represent the current state of the conversation.  The current
+  # state of a conversation consists of the latest version of the text of any
+  # "wrote" events that haven't been deleted or moved into a different
+  # conversation or sidebar, plus notices of any non-text events, such as
+  # retitling the conversation or changing who has access to the conversation.
+  #
+  # Consecutive conversation pieces that represent the same type of event -
+  # deleting, sidebaring, etc (with the exception of "wrote" events) will be
+  # collapsed into a single conversation piece.
+  #
+  # Returns an array of conversation pieces, in chronological order.
+  def pieces
+  end
 end
