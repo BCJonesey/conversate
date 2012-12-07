15470843e79a9e924e7cf20eaebb278078418d5c
Sean Kermes <skermes@gmail.com>
2012-12-07 03:22:24 -0500

    Adding config option to (maybe) make heroku work.
    From http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,8 @@ module Conversate
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    # Play nice with Heroku's slug compilation
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
