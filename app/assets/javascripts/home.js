7940d4ad638384c644aace989c8420e1cc84af1a
Sean Kermes <skermes@gmail.com>
2012-12-12 23:57:08 -0500

    First draft of carousel and door.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+  var carousel = $(".carousel");
+  var horses = $(".horse");
+  var index = 0;
+  var next = function() {
+    $(horses[index]).removeClass("riding");
+    index = (index + 1) % horses.length;
+    $(horses[index]).addClass("riding");
+    carousel.css("width", $(horses[index]).innerWidth() + 120 + "px");
+  };
+
+  next();
+  setInterval(next, 30000);
+})
