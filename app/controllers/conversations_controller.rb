b0b1bc5ec06ab7dce5cf498293baaff607ff6361
Sean Kermes <skermes@gmail.com>
2012-12-21 14:35:54 -0500

    Using single view for conversations.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
   before_filter :require_login
 
   def index
-    @conversations = current_user.conversations.order('updated_at DESC')
+    render_conversation_view
   end
 
   def new
@@ -11,21 +11,14 @@ class ConversationsController < ApplicationController
 
   def create
     @conversation_errors = []
+    conversation = nil
     if params[:users].length > 0
       users = User.find params[:users]
       conversation = Conversation.new({:users => (users << current_user).uniq,
                                        :title => params[:title]})
       @conversation_errors << 'Failed to save conversation' unless conversation.save
 
-      if params[:title]
-        title_event = Event.new({:conversation_id => conversation.id,
-                                 :user_id => current_user.id,
-                                 :event_type => 'retitle',
-                                 :data => {:title => conversation.title}.to_json})
-        @conversation_errors << 'Failed to save title event' unless title_event.save
-      end
-
-      if params[:first_message]
+      unless params[:first_message].empty?
         message_event = Event.new({:conversation_id => conversation.id,
                                    :user_id => current_user.id,
                                    :event_type => 'message',
@@ -40,51 +33,57 @@ class ConversationsController < ApplicationController
     if @conversation_errors.length > 0
       render :new
     else
-      @conversations = current_user.conversations.order('updated_at DESC')
-      render :index
+      render_conversation_view conversation
     end
   end
 
   def show
-    @conversation = Conversation.find(params[:id])
+    render_conversation_view Conversation.find(params[:id])
   end
 
   def retitle
-    @conversation = Conversation.find(params[:id])
-    render :show and return unless params[:title]
+    conversation = Conversation.find(params[:id])
+    render_conversation_view(conversation) and return if params[:title].empty?
 
-    @conversation.title = params[:title]
-    title_event = Event.new({:conversation_id => @conversation.id,
+    conversation.title = params[:title]
+    title_event = Event.new({:conversation_id => conversation.id,
                              :user_id => current_user.id,
                              :event_type => 'retitle',
                              :data => {:title => params[:title]}.to_json})
-    @conversation.save
+    conversation.save
     title_event.save
-    render :show
+    render_conversation_view conversation
   end
 
   def write
-    @conversation = Conversation.find(params[:id])
-    render :show and return unless params[:text]
+    conversation = Conversation.find(params[:id])
+    render_conversation_view(conversation) and return if params[:text].empty?
 
-    message_event = Event.new({:conversation_id => @conversation.id,
+    message_event = Event.new({:conversation_id => conversation.id,
                                :user_id => current_user.id,
                                :event_type => 'message',
-                               :data => {:message_id => @conversation.next_message_id,
+                               :data => {:message_id => conversation.next_message_id,
                                          :text => params[:text]}.to_json})
     message_event.save
-    render :show
+    render_conversation_view conversation
   end
 
   def delete
-    @conversation = Conversation.find(params[:id])
-    render :show and return unless params[:message]
+    conversation = Conversation.find(params[:id])
+    render_conversation_view(conversation) and return if params[:message].empty?
 
-    delete_event = Event.new({:conversation_id => @conversation.id,
+    delete_event = Event.new({:conversation_id => conversation.id,
                               :user_id => current_user.id,
                               :event_type => 'deletion',
                               :data => {:message_id => params[:message].to_i}.to_json})
     delete_event.save
-    render :show
+    render_conversation_view conversation
+  end
+
+  private
+  def render_conversation_view(conversation=nil)
+    @conversations = current_user.conversations.order('updated_at DESC')
+    @opened_conversation = conversation
+    render :index
   end
 end
