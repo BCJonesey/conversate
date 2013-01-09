a6f586be1962628750868c58daa0a76cb8b12f8b
Sean Kermes <skermes@gmail.com>
2013-01-09 09:20:56 -0800

    Not referencing nonexistent tag in js.

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,13 +1,17 @@
 (function() {
-  $(function() {
+  var setupTitleEditor = function() {
     var title = $("#column-conversation .title");
     var titleEditor = title.find("input[type='text']");
+
+    // TODO: Once this is done via AJAX instead of page refresh, update
+    // currentTitle on title change.
+    var currentTitle = titleEditor.val();
     title.on("click", function() {
       title.addClass("editing");
       titleEditor.focus();
     });
     titleEditor.on("blur", function() {
-      if (titleEditor.val() == title.find(".text").text()) {
+      if (titleEditor.val() == currentTitle) {
         title.removeClass("editing");
       }
       else {
@@ -16,13 +20,17 @@
     });
     titleEditor.on("keydown", function(e) {
       if (e.keyCode == 13) { // Enter
-        if (titleEditor.val() == title.find(".text").text()) {
+        if (titleEditor.val() == currentTitle {
           e.stopPropagation();
           e.preventDefault();
           title.removeClass("editing");
         }
       }
     });
+  }
+
+  $(function() {
+    setupTitleEditor();
 
     $(".conversation-piece.message").on("click", function(e) {
       var target = $(e.target);
