cfbee0c3232d3e67a960fc12493e17d5f2f038b1
William Lubelski <will.lubelski@gmail.com>
2013-01-11 16:00:25 -0500

    remove code to expand actions area of messages

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -24,27 +24,6 @@
       }
     });
 
-    $(".conversation-piece.message").on("click", function(e) {
-      var target = $(e.target);
-      if (!target.hasClass("conversation-piece")) {
-        target = target.parents(".conversation-piece").first();
-      }
-
-      if (target.hasClass("active")) {
-        e.stopPropagation();
-        return;
-      }
-
-      target.addClass("active");
-      target.find(".message-actions").slideDown(250, function() {
-        $(window).on("click", function() {
-          target.removeClass("active");
-          target.find(".message-actions").slideUp(250);
-          $(window).off("click");
-        });
-      });
-    });
-
     $("#compose textarea").on("keydown", function(e) {
       if (e.keyCode == 13) { // Enter
         $("#compose form").submit();
