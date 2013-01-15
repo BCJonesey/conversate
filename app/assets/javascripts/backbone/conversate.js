f454c87d6053eb5e3b848787b6af83d0206db397
Nick Martin <martin.nick@gmail.com>
2013-01-14 21:00:33 -0500

    Something approaching an initializer.

--- app/assets/javascripts/backbone/conversate.js
+++ app/assets/javascripts/backbone/conversate.js
@@ -3,3 +3,15 @@
 #= require_tree ./models
 #= require_tree ./views
 #= require_tree ./routers
+
+var ConversateApp = {
+	Models: {},
+	Collections: {},
+	Views: {},
+	Routers: {},
+	initialize: function(data) {
+		var conversations = new ConversateApp.Collections.Conversations(data.conversations);
+		new ConversateApp.Routers.Conversations({ conversations: conversations});
+		Backbone.history.start();
+	}
+}
