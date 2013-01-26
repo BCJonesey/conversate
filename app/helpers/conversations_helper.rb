571c8b5ed964de3f5e3e4b7fd663f2e86dbd3903
Sean Kermes <skermes@gmail.com>
2013-01-26 13:03:55 -0800

    Splitting message rendering into partials

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -19,4 +19,20 @@ module ConversationsHelper
     unread = logged_in? && current_user.unread_count > 0
     "<link rel=\"icon\" type=\"image/png\" href=\"/assets/watercooler#{unread ? '-unread' : ''}.png\">".html_safe
   end
+
+  def render_conversation_piece(piece)
+    if piece.type == :message
+      partial = 'message_piece'
+    elsif piece.type == :retitle
+      partial = 'retitle_piece'
+    elsif piece.type == :deletion
+      partial = 'deletion_piece'
+    elsif piece.type == :update_users
+      partial = 'update_users_piece'
+    else
+      partial = 'unknown_piece'
+    end
+
+    render :partial => "conversations/#{partial}", :locals => { :piece => piece }
+  end
 end
