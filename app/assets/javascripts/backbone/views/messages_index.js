727f4942da0bc06d9b4be9f69a23f4d874288d45
Nick Martin <martin.nick@gmail.com>
2013-01-31 18:48:00 -0500

    Name helper.

--- app/assets/javascripts/backbone/views/messages_index.js
+++ app/assets/javascripts/backbone/views/messages_index.js
@@ -1,11 +1,17 @@
 ConversateApp.Views.MessagesIndex = Backbone.View.extend({
   render: function () {
-    this.$el.html(JST['backbone/templates/messages/index']({ messages: this.collection }));
+    this.$el.html(JST['backbone/templates/messages/index']({ messages: this.collection,
+                                                            helpers: this.helpers }));
     return this;
   },
   initialize: function () {
         _.bindAll(this, 'render');
 
         this.collection.bind('all', this.render);
+  },
+  helpers: {
+    name: function (name) {
+      return name.full_name || name.email;
     }
+  }
 });
