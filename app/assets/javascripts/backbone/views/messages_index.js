d6f5fa859724085de2e84ce94219611588389b01
Nick Martin <martin.nick@gmail.com>
2013-02-03 16:53:48 -0500

    Quasi-working autoscroll. Doesn't turn off when not at bottom yet.

--- app/assets/javascripts/backbone/views/messages_index.js
+++ app/assets/javascripts/backbone/views/messages_index.js
@@ -2,8 +2,10 @@ ConversateApp.Views.MessagesIndex = Backbone.View.extend({
   render: function () {
     var self = this;
     self.collection.each(function (message) {
-      self.$el.append(JST['backbone/templates/messages/index']({ message: message, helpers: self.helpers }))
+      self.$el.append(JST['backbone/templates/messages/index']({ message: message,
+                                                                  helpers: self.helpers }))
     });
+
     return self;
   },
   initialize: function () {
@@ -12,11 +14,26 @@ ConversateApp.Views.MessagesIndex = Backbone.View.extend({
         this.collection.bind('add', this.add);
   },
   add: function (message) {
-    var autoscroll = (this.$el.scrollTop() == this.$el.scrollHeight());
-    this.$el.append(JST['backbone/templates/messages/index']({ message: message, helpers: this.helpers }))
+
+    var scrollable = $('#thread');
+    var autoscroll = this.autoscroll(scrollable);
+
+    this.$el.append(JST['backbone/templates/messages/index']({ message: message,
+                                                                helpers: this.helpers }))
+
+    // For unknown reasons, this.$el's scrollTop is broken.
     if (autoscroll) {
-      this.$el.scrollTop(this.$el.scrollHeight());
+      scrollable.scrollTop(scrollable[0].scrollHeight);
     }
+  },
+  autoscroll: function (scrollable) {
+
+    // There's a consistent difference between scrollHeight
+    // and scrollTop + height that we need to account for.
+    var scrollOffset = scrollable[0].scrollHeight - (scrollable.scrollTop() + scrollable.height());
+
+    return ((scrollable.scrollTop() + scrollable.height() + scrollOffset)
+                        == scrollable[0].scrollHeight);
   },
   helpers: {
     name: function (name) {
