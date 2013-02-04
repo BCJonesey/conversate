d9017c54aa0ddad70ed1378866b994512d37d3c0
Sean Kermes <skermes@gmail.com>
2013-02-04 09:43:07 -0800

    Enhance tweets.
    This involved retooling how the enhancifications get applied,
    because we have to get the twitter enhance from a cross-domain
    request, which is neccesarily asynchronous.  I ended up solving
    it by passing continuations around that loop us through
    everything.  It's not the prettiest solution, but it works.

--- app/assets/javascripts/enhancify.js
+++ app/assets/javascripts/enhancify.js
@@ -1,21 +1,71 @@
 (function() {
-  var imgify = function(match) {
-    return '<div class="image-in-message"><img src="' + match.trim() + '"></img></div>';
+  var imgify = function(match, continuation) {
+    continuation('<div class="image-in-message"><img src="' + match.trim() + '"></img></div>');
   };
 
-  var linkify = function(match) {
-    return ' <a href="' + match.trim() + '">' + match.trim() + '</a>';
+  var tweetify = function(match, continuation) {
+    // JSONP requests don't have any error/complete callback.
+    var error = setTimeout(function() {
+      linkify(match, continuation);
+    }, 5000);
+
+    $.ajax('https://api.twitter.com/1/statuses/oembed.json?url=' + match.trim(),
+           {crossDomain: true,
+            dataType: 'jsonp',
+            success: function(data) {
+              clearTimeout(error);
+              if (data.type == 'rich') {
+                continuation(data.html);
+              }
+              else {
+                linkify(match, continuation);
+              }
+            }
+           });
+  };
+
+  var linkify = function(match, continuation) {
+    var prefix = /^\s/.test(match) ? ' ' : '';
+    continuation(prefix + '<a href="' + match.trim() + '">' + match.trim() + '</a>');
   };
 
   var enhancers = [
     {regex: /(\s+|^)https?:[^\s]+\.(jpg|jpeg|png|gif)/gi, enhance: imgify},
+    {regex: /(\s+|^)https?:\/\/twitter\.com\/\S+\/status\/\d+/gi, enhance: tweetify},
     {regex: /(\s+|^)https?:[^\s]+/gi, enhance: linkify}
   ];
 
   $('.conversation-piece.message .message-text').each(function(messageIndex, text) {
-    enhancers.forEach(function(enhancer) {
-      var enhanced = text.innerHTML.replace(enhancer.regex, enhancer.enhance);
-      text.innerHTML = enhanced;
-    });
+    var nextEnhancer = function(enhancerIndex, enhancedHTML) {
+      if (enhancerIndex >= enhancers.length) {
+        $(text).html($.parseHTML(enhancedHTML, document, true));
+        return;
+      }
+
+      var enhancer = enhancers[enhancerIndex];
+      // Note that |splits| == |matches| + 1.
+      var matches = enhancedHTML.match(enhancer.regex);
+      if (!matches) {
+        nextEnhancer(enhancerIndex + 1, enhancedHTML);
+        return;
+      }
+
+      var splits = enhancedHTML.split(enhancer.regex);
+      var i = 0;
+      var enhanced = splits[0];
+      var continuation = function(replacement) {
+        enhanced += replacement;
+        i++;
+        enhanced += splits[i]
+        if (i < matches.length) {
+          enhancer.enhance(matches[i], continuation);
+        }
+        else {
+          nextEnhancer(enhancerIndex + 1, enhanced);
+        }
+      }
+      enhancer.enhance(matches[0], continuation);
+    }
+    nextEnhancer(0, text.innerHTML);
   });
 })();
