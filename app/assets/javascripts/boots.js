85eb0bb29a8dfd391ed1e32ba6479708986bb60f
William Lubelski <will.lubelski@gmail.com>
2013-02-04 20:55:04 -0500

    remove dropdown code and refactor message css

--- app/assets/javascripts/boots.js
+++ app/assets/javascripts/boots.js
@@ -1,11 +1,3 @@
-$('.btn.dropdown-toggle').click(function (e) {
-    e.stopPropagation();
-    $('.dropdown-menu, .popover').addClass('hidden');
-    $('.active').removeClass('active');
-    $(this).toggleClass('active').siblings().toggleClass('hidden');
-    $('html').on('click', function () {
-        $('.btn.dropdown-toggle').removeClass('active');
-        $('.dropdown-menu, .popover').addClass('hidden');
-        $('html').off('click');
-    });
+$('.message-delete').click(function(e){
+  $(this).parents('.message').find('form').submit();
 });
