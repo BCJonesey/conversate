f3f02b3a04c26bfd5827b90d40f2002e395091dd
Nick Martin <martin.nick@gmail.com>
2013-02-16 15:05:00 -0500

    Conversation-specific event index.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,11 @@
 class EventsController < ApplicationController
+  def index
+    if !!params[:id]
+      conversation = Conversation.find(params[:id])
+      current_user.mark_as_read(conversation)
+      render :json => conversation.pieces.to_json
+    else
+      render :json => ''
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Conversate::Application.routes.draw do
   put 'conversations/:id/write' => 'conversations#write', :as => 'write_message'
   put 'conversations/:id/delete' => 'conversations#delete', :as => 'delete_message'
   put 'conversations/:id/updateusers' => 'conversations#update_users', :as => 'update_users'
+  resources :events, :only => [:index]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
