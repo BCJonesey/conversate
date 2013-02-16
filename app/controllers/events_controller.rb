e8af6126dfedf00a7fcfaec4549386326de07711
Nick Martin <martin.nick@gmail.com>
2013-02-16 15:48:13 -0500

    Create action for events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,23 @@ class EventsController < ApplicationController
       render :json => ''
     end
   end
+
+  def create
+    if !!params[:id]
+      conversation = Conversation.find(params[:id])
+
+      message_event = Event.new({:conversation_id => conversation.id,
+                                 :user_id => current_user.id,
+                                 :event_type => 'message',
+                                 :data => {:message_id => conversation.next_message_id,
+                                           :text => params[:text]}.to_json})
+      message_event.save
+      current_user.mark_as_read(conversation)
+      render_conversation_view conversation
+      render :json => message_event.to_json
+    else
+      render :json => ''
+    end
+  end
+
 end
