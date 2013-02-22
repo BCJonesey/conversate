7dd8b90d7251709f08a180ca18e3e7a79d49f766
Nick Martin <martin.nick@gmail.com>
2013-02-21 21:55:11 -0500

    1m conversation updates.

--- app/assets/javascripts/backbone/conversate.js
+++ app/assets/javascripts/backbone/conversate.js
@@ -26,23 +26,28 @@ var ConversateApp = {
 		Backbone.history.start();
 
 		setInterval(function () {
-			console.log('fetch');
-    		self.conversations.fetch();
+			console.log('fetch convos');
+			self.conversations.fetch();
+
+			// This bit should mimic the behavior of unread_count_string + favicon stuff.
+			// Not ideal, but works for now.
+			var uc = $('.unread').length;
+			if (uc > 0) {
+				document.title = uc + ' - Water Cooler'
+				$('link[rel="icon"]').attr('href', '/assets/watercooler-unread.png');
+			}
+			else {
+				document.title = 'Water Cooler'
+				$('link[rel="icon"]').attr('href','/assets/watercooler.png');
+			}
+
+		}, 60000);
+
+		setInterval(function () {
+			console.log('fetch messages');
 
 				if (self.opened_conversation) {
 					self.messages.fetch({update: true, data: $.param({ id: opened_conversation }) });
-				}
-
-    		// This bit should mimic the behavior of unread_count_string + favicon stuff.
-    		// Not ideal, but works for now.
-				var uc = $('.unread').length;
-    		if (uc > 0) {
-					document.title = uc + ' - Water Cooler'
-					$('link[rel="icon"]').attr('href', '/assets/watercooler-unread.png');
-				}
-				else {
-					document.title = 'Water Cooler'
-					$('link[rel="icon"]').attr('href','/assets/watercooler.png');
 				}
 
 		}, 5000);
