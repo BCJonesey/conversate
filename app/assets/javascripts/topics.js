61949ce1e9b7b830480698d396cae735251dc035
Sean Kermes <skermes@gmail.com>
2013-02-24 12:44:36 -0800

    Remove commented code.

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,16 +1,10 @@
 (function() {
-
   function topicNew(){
     $('.topic-new').removeClass('hidden')
     $('.topic-new-input').focus().val('');
   }
 
-  // function topicEditStop(){
-  //   $('.topic-new-input').val('New Topic');
-  // }
-
   $(function() {
     $('.topic-new-button').on('click', topicNew);
-    //$('.topic-new-input').on('blur', topicEditStop);
   });
 })();
