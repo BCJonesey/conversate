6dd579622a136afff564efda2ff728935b342a8c
Sean Kermes <skermes@gmail.com>
2013-02-24 12:45:15 -0800

    Reformat code.

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,8 +1,8 @@
 (function() {
-  function topicNew(){
+  var topicNew = function() {
     $('.topic-new').removeClass('hidden')
     $('.topic-new-input').focus().val('');
-  }
+  };
 
   $(function() {
     $('.topic-new-button').on('click', topicNew);
