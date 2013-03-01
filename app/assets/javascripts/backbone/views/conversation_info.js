e1e75e158e236dd050fe17d20c4e2107a5c7e153
Nick Martin <martin.nick@gmail.com>
2013-02-28 21:09:02 -0500

    Rendering participants from json participant_tokens.

--- app/assets/javascripts/backbone/views/conversation_info.js
+++ app/assets/javascripts/backbone/views/conversation_info.js
@@ -1,10 +1,19 @@
 ConversateApp.Views.ConversationInfo = Backbone.View.extend({
   render: function () {
-    console.log(this.options.conversation);
-    this.$el.html(JST['backbone/templates/conversations/conversation-info']({ conversation: this.options.conversation }));
+    this.$el.html(JST['backbone/templates/conversations/conversation-info'](
+      {
+        conversation: this.options.conversation,
+        helpers: this.helpers
+      }));
     return this;
   },
   initialize: function () {
         _.bindAll(this, 'render');
+  },
+  helpers: {
+    // TODO DRY with messages_index view.
+    name: function (name) {
+      return name.full_name || name.email;
+    }
   }
 });
