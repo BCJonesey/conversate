d73d698e05f7e2737205b7ba7ab62f4d2209dd35
Sean Kermes <skermes@gmail.com>
2013-03-04 10:12:56 -0800

    Kill all but link enhancing.

--- app/assets/javascripts/enhancify.js
+++ app/assets/javascripts/enhancify.js
@@ -105,14 +105,15 @@
   };
 
   var enhancers = [
-    {regex: /(^|[^"])https?:[^\s]+\.(jpg|jpeg|png|gif)/gi, enhance: imgify},
-    {regex: /(^|[^"])https?:\/\/twitter\.com\/\S+\/status\/\d+/gi, enhance: tweetify},
-    {regex: /(?:^|[^"])https?:\/\/www\.youtube\.com\/watch\?[^\s,.!?()]+/gi, enhance: youtubify},
-    {regex: /(?:^|[^"])https?:\/\/vimeo\.com\/\d+/gi, enhance: vimeofy},
-    {regex: /(?:^|[^"])https?:\/\/soundcloud\.com\/\S+\/\S+/gi, enhance: soundcloudify},
+    // FUCK EVERYTHING
+    // {regex: /(^|[^"])https?:[^\s]+\.(jpg|jpeg|png|gif)/gi, enhance: imgify},
+    // {regex: /(^|[^"])https?:\/\/twitter\.com\/\S+\/status\/\d+/gi, enhance: tweetify},
+    // {regex: /(?:^|[^"])https?:\/\/www\.youtube\.com\/watch\?[^\s,.!?()]+/gi, enhance: youtubify},
+    // {regex: /(?:^|[^"])https?:\/\/vimeo\.com\/\d+/gi, enhance: vimeofy},
+    // {regex: /(?:^|[^"])https?:\/\/soundcloud\.com\/\S+\/\S+/gi, enhance: soundcloudify},
     // TODO: This color regex seems to cause lots of problems.  Fix it at some point.
     //{regex: /(?:^|\s)(#[0-9A-fa-f]{6}|#[0-9A-Fa-f]{3}|(?:rgb|hsl)a?)\([,\d%\s\.]+\)/gi, enhance: colorify},
-    {regex: /(?:^|[^"])http:\/\/codepen\.io\/\S+\/(?:pen|full)\/\S+/gi, enhance: codepenify},
+    // {regex: /(?:^|[^"])http:\/\/codepen\.io\/\S+\/(?:pen|full)\/\S+/gi, enhance: codepenify},
     {regex: /(?:^|[^"])https?:[^\s]+[^.,!?\s]/gi, enhance: linkify}
   ];
 
