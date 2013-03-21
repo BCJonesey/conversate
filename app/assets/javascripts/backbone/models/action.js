669143e5be01e6e99f36a36d273238c05e09a074
Sean Kermes <skermes@gmail.com>
2013-03-21 11:33:56 -0700

    Put human-friendly timestamps on messages.

--- app/assets/javascripts/backbone/models/action.js
+++ app/assets/javascripts/backbone/models/action.js
@@ -2,5 +2,40 @@ Structural.Models.Action = Backbone.Model.extend({
   initialize: function(attributes, options) {
     // TODO: Figure out where the current user is stored, update this.isOwnAction.
     this.isOwnAction = false;
-  }
+  },
+  humanizedTimestamp: (function() {
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug',
+                  'Sep', 'Oct', 'Nov', 'Dec'];
+    return function(relativeTo) {
+      var stamp = new Date(this.attributes.timestamp);
+
+      if (stamp.getFullYear() == relativeTo.getFullYear() &&
+          stamp.getMonth() == relativeTo.getMonth() &&
+          stamp.getDate() == relativeTo.getDate()) {
+        var hours = stamp.getHours();
+        var meridian = 'am';
+        if (hours >= 12) {
+          meridian = 'pm';
+          hours -= 12;
+        }
+
+        if (hours == 0) {
+          hours = 12;
+        }
+
+        var minutes = stamp.getMinutes();
+        if (minutes < 10) {
+          minutes = '0' + minutes;
+        }
+
+        return hours + ':' + minutes + ' ' + meridian;
+      }
+      else if (stamp.getFullYear() == relativeTo.getFullYear()) {
+        return months[stamp.getMonth()] +  ' ' + stamp.getDate();
+      }
+      else {
+        return (stamp.getMonth() + 1) + '/' + stamp.getDate() + '/' + (stamp.getFullYear() + '').substring(2);
+      }
+    };
+  })()
 });
