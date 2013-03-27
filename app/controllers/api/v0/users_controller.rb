22b7a412531209fd8e834cd337ea3cbcd5ff5a8a
Nick Martin <martin.nick@gmail.com>
2013-03-27 19:43:07 -0400

    Dummy users controller.

---
+++ app/controllers/api/v0/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::V0::UsersController < ApplicationController
+
+  # This is going to be kind of weird in that it shows data about the current_user
+  # rather than all users.
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end
