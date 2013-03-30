e306747d895730e0b75d5d58552088c424c07803
Nick Martin <martin.nick@gmail.com>
2013-03-30 14:42:24 -0400

    Comment about slight strangeness of conversations index controller.

--- app/controllers/api/v0/conversations_controller.rb
+++ app/controllers/api/v0/conversations_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::ConversationsController < ApplicationController
 
+  # Note that this is always on a url like /topics/1/conversations.
   def index
     topic = current_user.topics.find(params[:topic_id])
     render :json => topic.conversations.to_json
