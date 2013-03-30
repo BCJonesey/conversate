3dd00a05a9a7a4d55333a5bfbeac9e673f81a1c9
Nick Martin <martin.nick@gmail.com>
2013-03-30 16:54:08 -0400

    Actions index spec.

--- spec/controllers/api/v0/actions_controller_spec.rb
+++ spec/controllers/api/v0/actions_controller_spec.rb
@@ -8,12 +8,23 @@ describe Api::V0::ActionsController do
                           :password => 'superDUPERsecretPassword')
     login_user
     conversation = @user.conversations.create()
-    conversation.actions.create(:type => :message, :text => 'After the final no')
-    conversation.actions.create(:type => :retitle, :title => 'There comes a yes?')
+    conversation.actions.create(:event_type => :message, :data => '{"text":"After the final no"}')
+    conversation.actions.create(:event_type => :retitle, :data => '{"title":"There comes a yes?"}')
   end
 
   describe 'GET #index' do
-    it 'successfully responds with a list of actions for a specified conversation'
+    it 'successfully responds with a list of actions for a specified conversation' do
+      get :index, :conversation_id => 1
+      expect(response).to be_success
+      expect(response.code).to eq("200")
+      body = JSON.parse(response.body)
+      expect(body[0]['id']).to eq(1)
+      expect(body[0]['type']).to eq('message')
+      expect(body[0]['data']).to eq('{"text":"After the final no"}')
+      expect(body[0]['id']).to eq(2)
+      expect(body[0]['type']).to eq('retitle')
+      expect(body[0]['data']).to eq('{"title":"There comes a yes?"}')
+    end
     it 'responds unsuccessfully when the conversation does not exist'
   end
 
