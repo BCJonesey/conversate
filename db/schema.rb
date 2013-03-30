6467b54742175fe92964fea96b8991b63fb774d1
Nick Martin <martin.nick@gmail.com>
2013-03-30 13:25:29 -0400

    Reversion of schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222051526) do
+ActiveRecord::Schema.define(:version => 20130330170203) do
 
   create_table "conversations", :force => true do |t|
     t.string   "title"
@@ -59,18 +59,22 @@ ActiveRecord::Schema.define(:version => 20130222051526) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                           :null => false
+    t.string   "email",                                              :null => false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",                                      :null => false
-    t.datetime "updated_at",                                      :null => false
+    t.datetime "created_at",                                         :null => false
+    t.datetime "updated_at",                                         :null => false
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.string   "full_name"
-    t.boolean  "is_admin",                     :default => false
+    t.boolean  "is_admin",                        :default => false
     t.integer  "invited_by"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
   end
 
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
 
 end
