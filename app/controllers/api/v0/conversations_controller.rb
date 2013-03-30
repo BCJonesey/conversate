13f5a3f55b1f63fc92a93a46e037fc237e3947d9
Nick Martin <martin.nick@gmail.com>
2013-03-30 15:16:04 -0400

    Taking care of missing topics/conversations.

--- app/controllers/api/v0/conversations_controller.rb
+++ app/controllers/api/v0/conversations_controller.rb
@@ -2,18 +2,21 @@ class Api::V0::ConversationsController < ApplicationController
 
   # Note that this is always on a url like /topics/1/conversations.
   def index
-    topic = current_user.topics.find(params[:topic_id])
+    topic = current_user.topics.find_by_id(params[:topic_id])
+    head :status => :not_found and return unless topic
     render :json => topic.conversations.to_json
   end
 
   def create
-    topic = current_user.topics.find(params[:topic_id])
+    topic = current_user.topics.find_by_id(params[:topic_id])
+    head :status => :not_found and return unless topic
     conversation = topic.conversations.create()
     render :json => conversation
   end
 
   def show
-    conversation = Conversation.find(params[:id])
+    conversation = Conversation.find_by_id(params[:id])
+    head :status => :not_found and return unless conversation
     render :json => conversation.to_json
   end
 
