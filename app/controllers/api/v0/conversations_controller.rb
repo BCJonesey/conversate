0d7c0ec90b5e38be99943c04ef15978084d948ba
Nick Martin <martin.nick@gmail.com>
2013-03-30 14:41:38 -0400

    Conversation index implementation.

--- app/controllers/api/v0/conversations_controller.rb
+++ app/controllers/api/v0/conversations_controller.rb
@@ -1,6 +1,8 @@
 class Api::V0::ConversationsController < ApplicationController
 
   def index
+    topic = current_user.topics.find(params[:topic_id])
+    render :json => topic.conversations.to_json
   end
 
   def create
