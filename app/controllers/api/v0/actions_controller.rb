0c5f076d4e60522f2a2b059e23aae86384d5f310
Nick Martin <martin.nick@gmail.com>
2013-03-30 17:20:57 -0400

    Handling missing conversation in action index.

--- app/controllers/api/v0/actions_controller.rb
+++ app/controllers/api/v0/actions_controller.rb
@@ -2,6 +2,7 @@ class Api::V0::ActionsController < ApplicationController
 
   def index
     conversation = current_user.conversations.find_by_id(params[:conversation_id])
+    head :status => 404 and return unless conversation
     render :json => conversation.actions.to_json
   end
 
