ddf358fc0a14ba30a90c9da3ccb8aff97118a18d
Nick Martin <martin.nick@gmail.com>
2013-04-04 17:58:27 -0400

    Spec for participants index and implementation.

--- app/controllers/api/v0/participants_controller.rb
+++ app/controllers/api/v0/participants_controller.rb
@@ -1,6 +1,8 @@
 class Api::V0::ParticipantsController < ApplicationController
 
   def index
+    conversation = Conversation.find(params[:conversation_id])
+    render :json => conversation.participants(current_user).to_json
   end
 
   def create
--- spec/controllers/api/v0/participants_controller_spec.rb
+++ spec/controllers/api/v0/participants_controller_spec.rb
@@ -18,7 +18,16 @@ describe Api::V0::ParticipantsController do
 
   describe 'GET #index' do
     it 'successfully responds with all of the participants in the current conversation' do
-
+      get :index, :conversation_id => 1
+      expect(response).to be_success
+      expect(response.code).to eq("200")
+      body = JSON.parse(response.body)
+      expect(body[0]['id']).to eq(2)
+      expect(body[0]['full_name']).to eq('Ragnar the Red')
+      expect(body[0]['email']).to eq('ragnar@example.com')
+      expect(body[1]['id']).to eq(3)
+      expect(body[1]['full_name']).to eq('Hurdle Turtle')
+      expect(body[1]['email']).to eq('hurdleturtle@example.com')
     end
     it 'unsuccessfully responds when the conversation does not exist'
   end
