aecf8bec433edf1bfc295a7706a1a09e7531bdc8
Sean Kermes <skermes@gmail.com>
2013-04-09 13:36:53 -0400

    Fix test classname.

--- spec/javascripts/views/structural_spec.js
+++ spec/javascripts/views/structural_spec.js
@@ -6,10 +6,12 @@ describe("Structural", function() {
       var view = new Structural.Views.StructuralBar({model: model});
       view.render();
 
+      console.log(view.$el.html());
+
       expect(view.$('.i-text').text()).toMatch(/Water Cooler/);
       expect(view.$('.i-text').text()).toMatch(/People/);
       expect(view.$('.i-text').text()).not.toMatch(/Admin/);
-      expect(view.$('.text').text()).toMatch(/Dee Dee Ramone/);
+      expect(view.$('.i-text').text()).toMatch(/Dee Dee Ramone/);
 
       user = { name: "Monte A. Melnick",
                is_admin: true };
@@ -20,7 +22,7 @@ describe("Structural", function() {
       expect(view.$('.i-text').text()).toMatch(/Water Cooler/);
       expect(view.$('.i-text').text()).toMatch(/People/);
       expect(view.$('.i-text').text()).toMatch(/Admin/);
-      expect(view.$('.text').text()).toMatch(/Monte A\. Melnick/);
+      expect(view.$('.i-text').text()).toMatch(/Monte A\. Melnick/);
     })
   });
 
