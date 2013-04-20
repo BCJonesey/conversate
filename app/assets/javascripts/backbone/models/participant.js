971250b3f8315b158f02c1c21cac5bd8d159d0d3
Sean Kermes <skermes@gmail.com>
2013-04-20 16:22:09 -0400

    Infer participant names.

--- app/assets/javascripts/backbone/models/participant.js
+++ app/assets/javascripts/backbone/models/participant.js
@@ -1,1 +1,7 @@
-Structural.Models.Participant = Backbone.Model.extend({});
+Structural.Models.Participant = Backbone.Model.extend({
+  initialize: function(attributes, options) {
+    if (!this.get('name')) {
+      this.set('name', this.get('full_name') || this.get('email'));
+    }
+  }
+});
