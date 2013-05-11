18b7624dd0658e302269888a877a8ad6ee0ecd31
Nick Martin <martin.nick@gmail.com>
2013-05-11 13:20:24 -0400

    Making the original date more consistent.

--- db/migrate/20130222010401_add_most_recent_event_to_conversations.rb
+++ db/migrate/20130222010401_add_most_recent_event_to_conversations.rb
@@ -1,5 +1,5 @@
 class AddMostRecentEventToConversations < ActiveRecord::Migration
   def change
-    add_column :conversations, :most_recent_event, :time, :default => Time.now
+    add_column :conversations, :most_recent_event, :time, :default => DateTime::parse('2000-01-01 01:07:19')
   end
 end
