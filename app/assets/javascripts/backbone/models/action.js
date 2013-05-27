ff646a4b0dd658b69fd50eb6934bff059d12d2b6
Sean Kermes <skermes@gmail.com>
2013-05-27 15:10:34 -0400

    Factor humanizedtimestamp into a mixin.

--- app/assets/javascripts/backbone/models/action.js
+++ app/assets/javascripts/backbone/models/action.js
@@ -37,43 +37,10 @@ Structural.Models.Action = Backbone.Model.extend({
   markUnread: function() {
     this.set('is_unread', true);
   },
-  humanizedTimestamp: (function() {
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug',
-                  'Sep', 'Oct', 'Nov', 'Dec'];
-    return function(relativeTo) {
-      var stamp = new Date(this.get('timestamp'));
-
-      if (stamp.getFullYear() == relativeTo.getFullYear() &&
-          stamp.getMonth() == relativeTo.getMonth() &&
-          stamp.getDate() == relativeTo.getDate()) {
-        var hours = stamp.getHours();
-        var meridian = 'am';
-        if (hours >= 12) {
-          meridian = 'pm';
-          hours -= 12;
-        }
-
-        if (hours == 0) {
-          hours = 12;
-        }
-
-        var minutes = stamp.getMinutes();
-        if (minutes < 10) {
-          minutes = '0' + minutes;
-        }
-
-        return hours + ':' + minutes + ' ' + meridian;
-      }
-      else if (stamp.getFullYear() == relativeTo.getFullYear()) {
-        return months[stamp.getMonth()] +  ' ' + stamp.getDate();
-      }
-      else {
-        return (stamp.getMonth() + 1) + '/' + stamp.getDate() + '/' + (stamp.getFullYear() + '').substring(2);
-      }
-    };
-  })(),
   delete: function(user) {
     this.set('type', 'deletion');
     this.set('user', user);
   }
 });
+
+_.extend(Structural.Models.Action.prototype, Support.HumanizedTimestamp('timestamp'));
---
+++ app/assets/javascripts/backbone/support/timestamps.js
@@ -0,0 +1,44 @@
+/* Takes the name of a property on a model (as a string) and returns an object
+   suitable for mixing into that model (via _.extend) that calculates a human-
+   friendly timestamp for that property.
+
+   The property given should return a number suitable for passing into
+   `new Date()`. */
+Support.HumanizedTimestamp = function(property) {
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+                'Sep', 'Oct', 'Nov', 'Dev'];
+  return {
+    humanizedTimestamp: function(relativeTo) {
+      relativeTo = relativeTo || new Date();
+      var date = new Date(this.get(property));
+
+      if(date.getFullYear() === relativeTo.getFullYear() &&
+         date.getMonth() === relativeTo.getMonth() &&
+         date.getDate() === relativeTo.getDate()) {
+        var hours = date.getHours();
+        var meridian = 'am';
+        if (hours >= 12) {
+          meridian = 'pm';
+          hours -= 12;
+        }
+
+        if (hours === 0) {
+          hours = 12;
+        }
+
+        var minutes = date.getMinutes();
+        if (minutes < 10) {
+          minutes = '0' + minutes;
+        }
+
+        return hours + ':' + minutes + ' ' + meridian;
+      }
+      else if (date.getFullYear() === relativeTo.getFullYear()) {
+        return months[date.getMonth()] + ' ' + date.getDate();
+      }
+      else {
+        return (date.getMonth() + 1) + '/' + date.getDate() + '/' + (date.getFullYear() + '').substring(2);
+      }
+    }
+  };
+}
