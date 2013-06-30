6a47136e262f3c35f44f3b8b2624e191b11706fc
Nick Martin <martin.nick@gmail.com>
2013-06-30 15:43:37 -0400

    Participants editor makes me a sad panda.

--- app/assets/javascripts/backbone/views/participants.js
+++ app/assets/javascripts/backbone/views/participants.js
@@ -7,14 +7,10 @@ Structural.Views.Participants = Support.CompositeView.extend({
     this.originalCollection = this.collection.clone();
   },
   render: function() {
-    if ($('.token-input').count > 0) {
-      this.reset();
-    } else {
-      this.$el.empty();
-      this.$el.append($(this.userReminder));
-      this.$el.append($(this.inputElement));
-      this.collection.each(this.renderParticipant, this);
-    }
+    this.$el.empty();
+    this.$el.append($(this.userReminder));
+    this.$el.append($(this.inputElement));
+    this.collection.each(this.renderParticipant, this);
     return this;
   },
   renderParticipant: function(participant) {
