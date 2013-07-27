300c8925eba3a62e4f74c0ce1fa2328dd0ad357e
Sean Kermes <skermes@gmail.com>
2013-07-27 12:57:29 -0400

    Disable the new topic button.
    We'll bring it back later, but the current course of action is to
    tear topics down to the smallest number of parts.

--- app/assets/javascripts/backbone/views/topic_toolbar.js
+++ app/assets/javascripts/backbone/views/topic_toolbar.js
@@ -10,6 +10,6 @@ Structural.Views.TopicToolbar = Support.CompositeView.extend({
   },
   newTopic: function(e) {
     e.preventDefault();
-    this.trigger('new_topic');
+    // TODO: Create a new topic.
   }
 });
