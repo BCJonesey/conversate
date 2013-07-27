b6a0f5120c38639407e7e13e41d73a9e54c0acab
Sean Kermes <skermes@gmail.com>
2013-07-27 11:42:22 -0400

    Order conversations in topics controller.
    We have to do this before picking which conversation to focus so
    that we make sure to get the top one in the conversations list.
    
    Note that if we change the sort order of the conversations in the
    UI later we'll have to change this too.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,7 @@ class TopicsController < ApplicationController
     topic = Topic.find(params[:id])
     @topics = Topic.all
     @conversations = current_user.conversations.where(:topic_id => topic.id)
+                                               .order('most_recent_event DESC')
     @conversation = @conversations.first
     @actions = @conversation ? @conversation.actions : nil
     @participants = @conversation ? @conversation.participants : nil
