19dd4d14c5a28e37796a6a64fef9596618ccfb0c
Sean Kermes <skermes@gmail.com>
2013-08-08 18:46:45 -0400

    Scroller views can check if they're at the bottom.

--- app/assets/javascripts/backbone/support/scroller.js
+++ app/assets/javascripts/backbone/support/scroller.js
@@ -4,5 +4,13 @@ Support.Scroller = {
     this.$el.animate({
       scrollTop: height
     }, 500);
+  },
+  isAtBottom: function() {
+    var belowScrollbar = this.el.scrollHeight -
+                         this.$el.outerHeight() - this.$el.scrollTop();
+
+    // For some reason, in the action list at least, that calculation gets us 1
+    // when we're scrolled to the bottom, not 0.  For now, we fudge.
+    return belowScrollbar <= 10;
   }
 }
