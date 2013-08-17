50bde9a22b37ef67b462ecea3a7e34c7fee74b67
William Lubelski <will.lubelski@gmail.com>
2013-08-17 15:55:42 -0400

    add some content to the marketing pages

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -4,4 +4,10 @@ $(function() {
     $('.login-modal').toggleClass('hidden');
     $('.login-modal-trigger').toggleClass('is-toggled');
   })
+
+  $('.sub-nav-item').click(function(e){
+    e.preventDefault();
+    $('.sub-nav-item.is-selected').removeClass('is-selected');
+    $(this).addClass('is-selected');
+  });
 });
