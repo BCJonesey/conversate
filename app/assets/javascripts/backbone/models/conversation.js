d3e4d216d98041dff0a62360539c6d1a55e7ba5d
Nick Martin <martin.nick@gmail.com>
2013-08-17 13:18:06 -0400

    Defaulting to unread_count on conversation when actions are not up to date.

--- app/assets/javascripts/backbone/models/conversation.js
+++ app/assets/javascripts/backbone/models/conversation.js
@@ -33,6 +33,10 @@ Structural.Models.Conversation = Backbone.Model.extend({
   },
 
   unreadCount: function() {
+    var count = this.actions.unreadCount();
+    if (count == 0) {
+      return this.get('unread_count');
+    }
     return this.actions.unreadCount();
   }
 });
