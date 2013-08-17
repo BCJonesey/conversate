9e5995b906af3a387b6255981cfb073802854bab
William Lubelski <will.lubelski@gmail.com>
2013-08-17 14:13:59 -0400

    clean up the sign-in form thingie

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -2,5 +2,6 @@ $(function() {
   $('.login-modal-trigger').on('click', function(e) {
     e.preventDefault();
     $('.login-modal').toggleClass('hidden');
+    $('.login-modal-trigger').toggleClass('is-toggled');
   })
 });
