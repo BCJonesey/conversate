9656732f01a20eb3091558395452e7fafaf85729
Sean Kermes <skermes@gmail.com>
2013-08-18 03:15:45 -0400

    Test that address books are correctly scoped.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,41 @@ describe User do
       expect(Topic.all.length).to eq(topic_count + 1)
     end
   end
+
+  def build_user(name)
+    User.build(full_name: name,
+               email: "#{name}@example.com",
+               password: 'a')
+  end
+
+  describe 'address books' do
+    it 'only contains users that share a group' do
+      alice = build_user 'Alice'
+      bob = build_user 'Bob'
+      catherine = build_user 'Catherine'
+      dave = build_user 'Dave'
+      eliza = build_user 'Eliza'
+      fletcher = build_user 'Fletcher'
+
+      friends = Group.new(name: 'Friends')
+      friends.users += [alice, bob, eliza, fletcher]
+      friends.save
+
+      coworkers = Group.new(name: 'Coworkers')
+      coworkers.users += [alice, dave, catherine, fletcher]
+      coworkers.save
+
+      bob.address_book.index{ |u| u['id'] == alice.id }.should be_true
+      bob.address_book.index{ |u| u['id'] == eliza.id }.should be_true
+      bob.address_book.index{ |u| u['id'] == fletcher.id }.should be_true
+      bob.address_book.length.should eq(3)
+
+      fletcher.address_book.index{ |u| u['id'] == alice.id }.should be_true
+      fletcher.address_book.index{ |u| u['id'] == bob.id }.should be_true
+      fletcher.address_book.index{ |u| u['id'] == catherine.id }.should be_true
+      fletcher.address_book.index{ |u| u['id'] == dave.id }.should be_true
+      fletcher.address_book.index{ |u| u['id'] == eliza.id }.should be_true
+      fletcher.address_book.index{ |u| u['id'] == fletcher.id }.should be_nil
+    end
+  end
 end
