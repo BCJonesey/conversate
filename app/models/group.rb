9396ca20dc9f0ce73164f303901b1e9b5678bd5a
Sean Kermes <skermes@gmail.com>
2013-08-18 03:44:21 -0400

    Refactor admin updating into model.

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,4 +8,26 @@ class Group < ActiveRecord::Base
     gps = GroupParticipation.where(group_id: self.id, group_admin: true)
     gps.map { |gp| User.find(gp.user_id) }
   end
+
+  # Given a list of user ids who should be admins, make any users who are
+  # not admins but are on the list admins, and make any users who are
+  # admins but not on the list not admins.
+  def change_admins(ids)
+    ids.each do |id|
+      user = User.find(id)
+      unless user.group_admin?(self)
+        gp = GroupParticipation.where(user_id: user.id, group_id: self.id).first
+        gp.group_admin = true
+        gp.save
+      end
+    end
+
+    self.admins.each do |admin|
+      unless ids.include? admin.id.to_s
+        gp = GroupParticipation.where(user_id: admin.id, group_id: self.id).first
+        gp.group_admin = false
+        gp.save
+      end
+    end
+  end
 end
