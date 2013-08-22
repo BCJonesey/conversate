c0ea52ff125d8deca7d0857635ba02d603da3c5d
Sean Kermes <skermes@gmail.com>
2013-08-22 17:41:36 -0400

    Skeleton for health checks.

---
+++ app/support/health.rb
@@ -0,0 +1,25 @@
+module Health
+  def Health.users_without_default_topics
+    []
+  end
+
+  def Health.users_without_any_topics
+    []
+  end
+
+  def Health.topics_without_any_users
+    []
+  end
+
+  def Health.conversations_without_any_users
+    []
+  end
+
+  def Health.conversations_without_any_topics
+    []
+  end
+
+  def Health.topics_with_no_admins
+    []
+  end
+end
