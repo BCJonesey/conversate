9adb877274d75f9dbad71a393b8d8a3cc26f5763
Sean Kermes <skermes@gmail.com>
2013-08-27 23:50:16 -0400

    Dismiss sign in modal on click away.

--- app/assets/javascripts/marketing.js
+++ app/assets/javascripts/marketing.js
@@ -1,10 +1,20 @@
 $(function() {
-  $('.mkt-sign-in-toggle').on('click', function(e) {
+  var toggle = $('.mkt-sign-in-toggle');
+  toggle.on('click', function(e) {
     e.preventDefault();
-    var toggle = $(e.target);
     var form = $('.login-modal');
 
     toggle.toggleClass('is-toggled');
     form.toggleClass('hidden');
+  });
+
+  // This way we don't attach a needless event on a page without this button.
+  toggle.parents('body').on('click', function(e) {
+    e.preventDefault();
+    var target = $(e.target);
+    if (toggle.hasClass('is-toggled') && target.closest('.login-modal, .mkt-sign-in-toggle').length === 0) {
+      toggle.toggleClass('is-toggled');
+      $('.login-modal').toggleClass('hidden');
+    }
   })
 })
