ab826d17322234aa83be79e02b133f90a79f2e18
William Lubelski <will.lubelski@gmail.com>
2013-09-22 15:53:23 -0400

    add the UI functions for deleting a folder

--- app/assets/javascripts/backbone/views/topic_editor.js
+++ app/assets/javascripts/backbone/views/topic_editor.js
@@ -19,13 +19,44 @@ Structural.Views.TopicEditor = Support.CompositeView.extend({
   },
 
   events: {
-    'click .ef-save-button': 'save'
+    'click .ef-save-button': 'save',
+    'click .ef-delete-button':'delete',
+    'click .ef-trash-button':'showDeleteWarning',
+    'click .ef-deletion-cancel':'hideDeleteWarning',
+    'click .ef-deletion-confirmation':'toggleDeleteButton'
   },
 
   show: function(topic) {
     this._topic = topic;
     this.render(topic);
     this.$('.modal-background').removeClass('hidden');
+  },
+  showDeleteWarning: function(e){
+    e.preventDefault();
+    this.$('.ef-window-content').addClass('hidden');
+    this.$('.ef-deletion-content').removeClass('hidden');
+  },
+  hideDeleteWarning: function(e){
+    e.preventDefault();
+    this.$('.ef-window-content').removeClass('hidden');
+    this.$('.ef-deletion-content').addClass('hidden');
+    this.hideDeleteButton();
+    this.$('.ef-deletion-confirmation').prop('checked', false);
+  },
+  showDeleteButton: function(e){
+    this.$('.ef-delete-button').removeClass('hidden');
+    this.$('.ef-save-button').addClass('hidden');
+  },
+  hideDeleteButton: function(e){
+    this.$('.ef-save-button').removeClass('hidden');
+    this.$('.ef-delete-button').addClass('hidden');
+  },
+  toggleDeleteButton: function(e){
+    if (this.$('.ef-deletion-confirmation').prop('checked') === true){
+      this.showDeleteButton();
+    } else {
+      this.hideDeleteButton();
+    }
   },
   save: function(e) {
     e.preventDefault();
