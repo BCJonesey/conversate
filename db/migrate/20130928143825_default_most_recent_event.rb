3f99435d64e4f8ee9da899d31eb925761ddfdf9b
Sean Kermes <skermes@gmail.com>
2013-09-28 11:41:04 -0400

    Use up/down methods.
    I thought that change would warn me if I ran it on an
    irreversible method, but apparently not.  Fuck you,
    rails.

--- db/migrate/20130928143825_default_most_recent_event.rb
+++ db/migrate/20130928143825_default_most_recent_event.rb
@@ -1,5 +1,9 @@
 class DefaultMostRecentEvent < ActiveRecord::Migration
-  def change
+  def up
     change_column_default :conversations, :most_recent_event, DateTime.new(2000, 1, 1, 1, 7, 19);
+  end
+
+  def down
+    change_column_default :conversations, :most_recent_event, nil
   end
 end
