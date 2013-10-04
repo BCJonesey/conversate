c49108a0ca83670bcb60e4647c0342461f4838cd
Sean Kermes <skermes@gmail.com>
2013-10-04 05:12:10 -0400

    Test the /status endpoint.

--- app/controllers/api/v0/statuses_controller.rb
+++ app/controllers/api/v0/statuses_controller.rb
@@ -2,8 +2,11 @@ class Api::V0::StatusesController < ApplicationController
   before_filter :require_login
 
   def show
-    render :json => {
-      global_most_recent_action: current_user.conversations.maximum(:most_recent_action)
-    }.to_json
+    status = {}
+    if current_user.conversations.length > 0
+      status[:global_most_recent_action] =
+        current_user.conversations.maximum(:most_recent_action).msec
+    end
+    render :json => status.to_json
   end
 end
