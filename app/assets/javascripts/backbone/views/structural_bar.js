0d2f059138ffa1c61d42facb65916524a4621e9e
William Lubelski <will.lubelski@gmail.com>
2013-10-12 15:19:47 -0400

    create a wc-news popover in the structural bar

--- app/assets/javascripts/backbone/views/structural_bar.js
+++ app/assets/javascripts/backbone/views/structural_bar.js
@@ -4,6 +4,15 @@ Structural.Views.StructuralBar = Support.CompositeView.extend({
   initialize: function(options) {
     options = options || {};
   },
+
+  events: {
+    'click .wc-news-toggle': 'toggleNews'
+  },
+
+  toggleNews: function(){
+    $(".wc-news-popover").toggleClass('hidden');
+  },
+
   render: function() {
     this.$el.html(this.template({user: this.model}));
     return this;
