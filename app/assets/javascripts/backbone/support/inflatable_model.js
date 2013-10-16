3c6bf68e1336d05f56ac08b64b44b43190b27128
Sean Kermes <skermes@gmail.com>
2013-10-16 13:33:53 -0400

    Explain how to use InflatableModel.

--- app/assets/javascripts/backbone/support/inflatable_model.js
+++ app/assets/javascripts/backbone/support/inflatable_model.js
@@ -1,3 +1,23 @@
+// Inflatable models have other Backbone collections or models as some of their
+// attributes.  In order to make an inflatable model, do three things.
+//
+// 1) Extend from Support.InflatableModel:
+//
+//    Structural.Models.Foo = Support.InflatableModel.extend({});
+//
+// 2) Implement inflateAttributes:
+//
+//    inflateAttributes: function(attrs) {
+//      attrs.bars = inflate(Structural.Collections.Bars, attrs.bars);
+//      return attrs;
+//    }
+//
+// 3) If you implement initialize or parse, include the appropriate super
+//    call:
+//
+//    this.constructor.__super__.initialize.call(this, attributes, options);
+//    response = this.constructor.__super__.parse.call(this, response);
+
 Support.InflatableModel = function(attributes, options) {
   Backbone.Model.apply(this, [attributes, options]);
 }
