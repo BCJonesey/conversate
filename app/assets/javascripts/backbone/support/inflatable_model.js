d7c6d92115638cdba4579633e63ea4d6e11f5bbe
Sean Kermes <skermes@gmail.com>
2013-10-16 13:20:07 -0400

    Implement chooseType function.

--- app/assets/javascripts/backbone/support/inflatable_model.js
+++ app/assets/javascripts/backbone/support/inflatable_model.js
@@ -15,6 +15,24 @@ _.extend(Support.InflatableModel.prototype, Backbone.Model.prototype, {
     } else {
       return new type(data);
     }
+  },
+
+  // Sometimes we need to do basically the same work but with a different
+  // inflated type; this is a convenient way to let us pick the type we're
+  // working with.  Note that the types get used like in inflate above:
+  //
+  // new x(data)
+  //
+  // The default function here this acts as an identity function.  When called
+  // with new, a function with no return value will implicitly return this, but
+  // if the function does explicitly return something, that value is used
+  // instead.
+  chooseType: function(map, key) {
+    if (map[key]) {
+      return map[key];
+    } else {
+      return function(x) { return x; };
+    }
   }
 });
 
