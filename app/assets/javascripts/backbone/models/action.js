07411efc4d1ab56b652711ca832ea188f642331e
Sean Kermes <skermes@gmail.com>
2013-10-18 14:43:32 -0400

    Mix InflatableModel into Action.

--- app/assets/javascripts/backbone/models/action.js
+++ app/assets/javascripts/backbone/models/action.js
@@ -1,93 +1,35 @@
 Structural.Models.Action = Backbone.Model.extend({
   initialize: function(attributes, options) {
-    if (this.get('user')) {
-      this.set('user', new Structural.Models.Participant(this.get('user')));
-
-      // When syncing with the server, the user property has a habit of
-      // being overwritten and not replaced with a model.
-      this.on('change:user', function() {
-        // I swear to god, when I inverted this condition it was executing the
-        // wrong path.  The next time you read this comment try re-doing this
-        // and maybe the stars will be in a better alignment.
-
-        // It's probably because it's a sub part of the json and needs to be parsed out
-        // as a real model. Not interested right now. - Nick
-        if (this.get('user') instanceof Structural.Models.Participant) {
-          // No-op
-        }
-        else {
-          this.set('user', new Structural.Models.Participant(this.get('user')));
-        }
-      }, this);
-    }
-
-    if ((this.get('type') == 'update_users' || this.get('type') == 'update_viewers') &&
-        (!this.get('added') || this.get('added').length === 0)) {
-      this.set('added', []);
-    }
-
-    if ((this.get('type') == 'update_users' || this.get('type') == 'update_viewers') &&
-        (!this.get('removed') || this.get('removed').length === 0)) {
-      this.set('removed', []);
-    }
-
-    if ((this.get('type') == 'update_users' || this.get('type') == 'update_viewers') &&
-        this.get('added') &&
-        this.get('removed')) {
-      this.set('added', new Structural.Collections.Participants(this.get('added')));
-      this.set('removed', new Structural.Collections.Participants(this.get('removed')));
-    }
-
-    if (this.get('type') === 'update_folders' &&
-        !this.get('added')) {
-      this.set('added', []);
-    }
-
-    if (this.get('type') === 'update_folders' &&
-        !this.get('removed')) {
-      this.set('removed', []);
-    }
-
-    if (this.get('type') === 'update_folders' &&
-        this.get('added') &&
-        this.get('removed')) {
-      this.set('added', new Structural.Collections.Folders(this.get('added')));
-      this.set('removed', new Structural.Collections.Folders(this.get('removed')));
-    }
-
-    if (this.get('type') == 'move_conversation') {
-      this.set('from', new Structural.Models.Folder(this.get('from')));
-      this.set('to', new Structural.Models.Folder(this.get('to')));
-    }
-
-    if (this.get('type') == 'move_message') {
-      this.set('from', new Structural.Models.Conversation(this.get('from')));
-      this.set('to', new Structrual.Models.Conversation(this.get('to')));
-    }
-
-    if (this.get('text')) {
-      this.set('enhanced_text', Support.TextEnhancer.enhance(_.escape(this.get('text'))));
-    }
+    this.inflateExtend(this.attributes);
   },
   parse: function (response, options) {
+    return this.inflateReturn(response);
+  },
 
-    // Special parsing for update_users actions.
-    if (response.added) {
-      response.added = _.map(response.added, function (p) {
-        return new Structural.Models.Participant(p);
-      });
-    }
-    if (response.removed) {
-      response.removed = _.map(response.removed, function (p) {
-        return new Structural.Models.Participant(p);
-      });
+  inflateAttributes: function(attrs) {
+    if (attrs.user) {
+      attrs.user = this.inflate(Structural.Models.Participant, attrs.user);
     }
 
-    if (response.text) {
-      response.enhanced_text = Support.TextEnhancer.enhance(_.escape(response.text));
+    if (_.contains(['update_users', 'update_folders'], attrs.type)) {
+      var collection = this.chooseType({
+        update_users: Structural.Collections.Participants,
+        update_folders: Structural.Collections.Folders
+      }, attrs.type);
+
+      attrs.added = this.inflate(collection, attrs.added);
+      attrs.removed = this.inflate(collection, attrs.removed);
     }
 
-    return response;
+    if (attrs.type === 'move_message') {
+      attrs.from = this.inflate(Structural.Models.Conversation, attrs.from);
+    }
+
+    if (attrs.text) {
+      attrs.enhanced_text = Support.TextEnhancer.enhance(_.escape(attrs.text));
+    }
+
+    return attrs;
   },
 
   focus: function() {
@@ -112,3 +54,4 @@ Structural.Models.Action = Backbone.Model.extend({
 });
 
 _.extend(Structural.Models.Action.prototype, Support.HumanizedTimestamp('timestamp'));
+_.extend(Structural.Models.Action.prototype, Support.InflatableModel);
