7ae59ff1ac1c57f0bfc046c073b78e29fad3ea7b
Sean Kermes <skermes@gmail.com>
2013-10-30 15:43:57 -0400

    Helper method to find reading logs.

--- app/models/reading_log.rb
+++ app/models/reading_log.rb
@@ -3,4 +3,11 @@ class ReadingLog < ActiveRecord::Base
   belongs_to :user
   validates :user_id, uniqueness: { scope: :conversation_id }
   attr_accessible :conversation_id, :user_id, :last_read_event
+
+  # This is a pretty terrible name, but the whole point of this method is to
+  # have a shorter name than the method it calls, and I don't have a better
+  # name than this.
+  def ReadingLog.get(user_id, conversation_id)
+    ReadingLog.find_by_user_id_and_conversation_id user_id, conversation_id
+  end
 end
