dce6b85223a7e7b8c675154f4559cb54a02048b9
Nick Martin <martin.nick@gmail.com>
2013-11-03 12:53:34 -0500

    Migration for adding archived column to reading logs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130929002028) do
+ActiveRecord::Schema.define(:version => 20131103174951) do
 
   create_table "actions", :force => true do |t|
     t.integer  "conversation_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20130929002028) do
     t.integer  "user_id"
     t.datetime "most_recent_viewed"
     t.integer  "unread_count",       :default => 0
+    t.boolean  "archived",           :default => false
   end
 
   add_index "reading_logs", ["conversation_id"], :name => "index_reading_logs_on_conversation_id"
