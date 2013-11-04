d4341d776d1e116bb5f49250321f9f80bb9d01ad
William Lubelski <will.lubelski@gmail.com>
2013-11-04 11:18:26 -0500

    add the cnv-section class and bind clicking stuff to toggling the collapsed state

--- app/assets/javascripts/backbone/views/conversations_section.js
+++ app/assets/javascripts/backbone/views/conversations_section.js
@@ -1,4 +1,5 @@
 Structural.Views.ConversationsSection = Support.CompositeView.extend({
+  className: 'cnv-section',
   initialize: function(options) {
     options = options || {};
     this.name = options.name;
@@ -8,11 +9,18 @@ Structural.Views.ConversationsSection = Support.CompositeView.extend({
     this.$el.html(this.template({name: this.name}));
     return this;
   },
+  events: {
+    'click .cnv-divider': 'toggleCollapsed',
+    'click .cnv-note': 'toggleCollapsed'
+  },
   renderConversation: function(conversation) {
     var view = new Structural.Views.Conversation({
       model: conversation,
       user: this.user
     });
     this.appendChild(view);
+  },
+  toggleCollapsed: function(e){
+    $(e.target).closest('.cnv-section').toggleClass('is-collapsed')
   }
 });
