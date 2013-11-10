322de44b81597f1cca7847b7c77f2b27ac381d57
Sean Kermes <skermes@gmail.com>
2013-11-10 14:46:46 -0500

    Email worker pulls items off email queue.

--- lib/tasks/workers/email_worker.rb
+++ lib/tasks/workers/email_worker.rb
@@ -15,9 +15,30 @@ class EmailWorker
     trap('INT') { log 'interrupted'; @exit = true }
 
     loop do
-      sleep SLEEP
+      pending_emails = EmailQueue.all
+
+      if pending_emails.count == 0
+        sleep SLEEP
+      else
+        pending_emails.each do |email|
+          user = User.find(email.external_user_id)
+          action = Action.find(email.action_id)
+          conversation = action.conversation
+          log "sending email to #{user.email}: action ##{action.id}"
+          EmailQueue.delete email
+          response = send_email user, action, conversation
+          log "response from #{user.email} for action ##{action.id}: #{response}"
+          # TODO: Handle errors in delivery
+
+          break if @exit
+        end
+      end
+
       break if @exit
     end
+  end
+
+  def send_email(user, action, conversation)
   end
 
   def log(text)
