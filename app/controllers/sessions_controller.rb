de1017ed38fe5203624d598b449de25639fe1270
Sean Kermes <skermes@gmail.com>
2013-11-10 11:42:16 -0500

    External users can't log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
-    if !user || user.removed
+    if !user || user.removed || user.external
       @login_error = true
       redirect_to root_path and return
     end
