e48b073bc8069644270ecb4e801f4b10d0a76448
Sean Kermes <skermes@gmail.com>
2013-11-10 23:14:08 -0500

    Make database report ignore non-db models.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,9 @@
 module AdminHelper
-  def all_models
+  def all_database_models
     Dir[Rails.root.join('app/models/*.rb').to_s].collect do |filename|
       klass = File.basename(filename, '.rb').camelize.constantize
       klass.ancestors.include?(ActiveRecord::Base) ? klass : nil
-    end
+    end.compact
   end
 
   def health_tests
