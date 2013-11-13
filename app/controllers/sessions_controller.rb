c52b38ddfb9e1f7c5a07cfe612f97ef8eba0ae59
Ben Jones <ben@cerberusentertainment.com>
2013-11-12 20:51:23 -0500

    initial error commit"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_email(params[:email])
-    if !user || user.removed || user.external
-      @login_error = true
-      redirect_to root_path and return
-    end
-
     user = login params[:email], params[:password], params[:remember_me]
     if user
-      redirect_back_or_to root_url
+      redirect_back_or_to root_url 
     else
       @login_error = true
+      flash[:alert] = "Failed to match e-mail/password"
       redirect_to root_path
     end
   end
