26af4b45c8eaea44ecc47d79bd4ae022064c3ac5
Ben Jones <ben@cerberusentertainment.com>
2013-11-23 13:12:10 -0500

    initial folder support

--- app/models/mandrill_inbound_email.rb
+++ app/models/mandrill_inbound_email.rb
@@ -1,7 +1,7 @@
 class MandrillInboundEmail
-  CNV_REGEX = /cnv-(\d+)@.*\.watercoolr\.io/
+  CNV_REGEX = /cnv-(\d+)@(.*)\.watercoolr\.io/
 
-  attr_reader :sender, :action, :conversation
+  attr_reader :sender, :action, :conversation, :subdomain, :domain, :recipient, :folder
 
   def initialize(data)
     @data = data
@@ -12,18 +12,39 @@ class MandrillInboundEmail
       user_id: self.sender_user.id
     )
     CNV_REGEX =~ @data['email']
-    match = $1
-    @conversation = Conversation.find(match) unless match.nil?
+    @recipient = $1
+    @subdomain = $2
+
+    case @recipient
+      when /cnv-(\d+)/
+        @conversation = Conversation.find($1)
+      when /(\d+)/
+        @folder = Folder.find($1)
+    end
   end
 
   def to_conversation?
     !@conversation.nil?
   end
 
+  def dispatch
+    if !@conversation.nil?
+      self.dispatch_to_conversation
+    elsif !folder.nil?
+      self.dispatch_to_folder
+    end
+  end
   def dispatch_to_conversation
     self.action.save
     self.conversation.actions << self.action
     # TODO: make handle deal with unread counts
     self.conversation.handle action
+  end
+
+  def dispatch_to_folder
+    @conversation =  @folder.conversations.create()
+    conversation.set_title params[:title]
+    conversation.add_participants [@sender], @sender
+    self.dispatch_to_conversation
   end
 end
