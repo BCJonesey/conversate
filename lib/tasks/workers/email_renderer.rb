2322c6d8873013722a1bd1d9ce9f58a2fd217567
Sean Kermes <skermes@gmail.com>
2013-12-04 05:40:47 -0500

    Include current_user variable in email renderer.

--- lib/tasks/workers/email_renderer.rb
+++ lib/tasks/workers/email_renderer.rb
@@ -3,19 +3,21 @@ module EmailRenderer
   # at the start of the separator.
   SEPARATOR = "----------------------------\n"
 
-  def EmailRenderer.render(conversation)
+  def EmailRenderer.render(conversation, current_user)
     relevant_actions = conversation.actions.order('created_at DESC').limit(30)
-    fragments = relevant_actions.map {|a| render_action(a) }
-    fragments.insert(0, render_participation_header(conversation))
+    fragments = relevant_actions.map {|a| render_action(a, current_user) }
+    fragments.insert(0, render_participation_header(conversation, current_user))
     fragments.join(SEPARATOR)
   end
 
-  def EmailRenderer.render_action(action)
-    render_template(action.type, {:action => action})
+  def EmailRenderer.render_action(action, current_user)
+    render_template(action.type, {:action => action,
+                                  :current_user => current_user})
   end
 
-  def EmailRenderer.render_participation_header(conversation)
-    render_template('participation_header', {:conversation => conversation})
+  def EmailRenderer.render_participation_header(conversation, current_user)
+    render_template('participation_header', {:conversation => conversation,
+                                             :current_user => current_user})
   end
 
   def EmailRenderer.render_template(template, data)
