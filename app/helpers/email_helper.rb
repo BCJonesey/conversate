655fe2c90a2bdfe79fb347bc8dbaa29ad5b5a383
William Lubelski <will.lubelski@gmail.com>
2013-12-14 21:47:45 -0500

    get dem styles lookin' good

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -4,6 +4,10 @@ module EmailHelper
     "1px solid #CCC"
   end
 
+  def quote_border
+    "1px solid #80A0C0"
+  end
+
   # There are some strings (like times) that email clients will unwantedly
   # enhance into clickable targets.  This gets around that.
   def spanify_characters(string)
