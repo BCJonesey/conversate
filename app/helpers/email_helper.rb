e7c06e243ab13eb60983f042e639afea0c2bdb23
Sean Kermes <skermes@gmail.com>
2013-12-14 19:05:09 -0500

    Spanify timestamps.
    This is kind of nonsense, but it should get around some annoying
    client behavior.

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,3 +1,7 @@
 module EmailHelper
-
+  # There are some strings (like times) that email clients will unwantedly
+  # enhance into clickable targets.  This gets around that.
+  def spanify_characters(string)
+    string.chars.map {|c| "<span>#{c}</span>" }.join.html_safe
+  end
 end
