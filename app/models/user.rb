e7c0ce60cb5cc60ab6aced340b3b6ff5ae1d39a5
Sean Kermes <skermes@gmail.com>
2013-12-18 17:00:47 -0500

    Validate email uniqueness without regard for case.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create, :unless => :external
   validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
 
   def self.build(params)
     user = User.new(params)
