b5ce101be0cdee1ae4dc785a1dc71811bf7b7f8b
Sean Kermes <skermes@gmail.com>
2014-01-05 06:29:16 -0500

    Put autocomplete after people list in new-cnv.

--- app/assets/javascripts/backbone/views/new_conversation.js
+++ app/assets/javascripts/backbone/views/new_conversation.js
@@ -21,8 +21,8 @@ Structural.Views.NewConversation = Support.CompositeView.extend({
     this.removableList.on('remove', this.autocomplete.removeFromBlacklist, this.autocomplete);
 
     this.$el.html(this.template());
-    this.insertChildBefore(this.autocomplete, this.$('.new-cnv-contents'));
     this.insertChildBefore(this.removableList, this.$('.new-cnv-contents'));
+    this.insertChildBefore(this.autocomplete, this.$('.new-cnv-contents'));
 
     return this;
   },
