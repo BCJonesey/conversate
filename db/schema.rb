d7955e5c7d1af2d869acb12dd74e674a54e2d670
Sean Kermes <skermes@gmail.com>
2014-01-25 12:30:43 -0500

    Bad migration for json column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215035807) do
+ActiveRecord::Schema.define(version: 20140125172901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20131215035807) do
     t.integer  "conversation_id"
     t.integer  "user_id"
     t.string   "type"
-    t.text     "data"
+    t.json     "data"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
