b5276d02dd64b7c78a6a1a84b2dc66a3d84ae496
Sean Kermes <skermes@gmail.com>
2014-01-25 14:26:21 -0500

    All tests passed.

--- spec/controllers/api/v0/folders_controller_spec.rb
+++ spec/controllers/api/v0/folders_controller_spec.rb
@@ -7,10 +7,10 @@ describe Api::V0::FoldersController do
                           :full_name => 'Rufio Pan',
                           :password => 'superDUPERsecretPassword')
     login_user
-    tt = Folder.create(:name => 'Terror Time')
-    p = Folder.create(:name => 'Plebians')
-    @user.folders << tt
-    @user.folders << p
+    @tt = Folder.create(:name => 'Terror Time')
+    @p = Folder.create(:name => 'Plebians')
+    @user.folders << @tt
+    @user.folders << @p
   end
 
   describe 'GET #index' do
@@ -19,9 +19,9 @@ describe Api::V0::FoldersController do
       expect(response).to be_success
       expect(response.code).to eq("200")
       body = JSON.parse(response.body)
-      expect(body[0]['id']).to eq(1)
+      expect(body[0]['id']).to eq(@tt.id)
       expect(body[0]['name']).to eq('Terror Time')
-      expect(body[1]['id']).to eq(2)
+      expect(body[1]['id']).to eq(@p.id)
       expect(body[1]['name']).to eq('Plebians')
     end
     it "successfully returns folders with correct default unread counts" do
@@ -43,7 +43,8 @@ describe Api::V0::FoldersController do
       expect(response).to be_success
       expect(response.code).to eq("201")
       body = JSON.parse(response.body)
-      expect(body['id']).to eq(3)
+      expect(body['id']).not_to eq(@tt.id)
+      expect(body['id']).not_to eq(@p.id)
       expect(body['name']).to eq('Huzzah!')
       expect(body['unread_conversations'].length).to eq 0
     end
@@ -51,22 +52,22 @@ describe Api::V0::FoldersController do
 
   describe 'DELETE #delete' do
     it "successfully deletes the specified folder" do
-      expect(Folder.find_by_id(1)).to be_true
-      delete :destroy, :id => 1
+      expect(Folder.find_by_id(@tt.id)).to be_true
+      delete :destroy, :id => @tt.id
       expect(response).to be_success
       expect(response.code).to eq("204")
-      expect(Folder.find_by_id(1)).to be_nil
+      expect(Folder.find_by_id(@tt.id)).to be_nil
     end
     it "cannot delete a user's default folder" do
       @user.default_folder_id = Folder.create!(:name => 'Unsinkable Folder').id
       @user.save
-      expect(User.find_by_id(1).default_folder_id).to eq(3)
+      expect(User.find_by_id(@user.id).default_folder_id).to eq(@user.default_folder_id)
 
-      expect(Folder.find_by_id(3)).to be_true
-      delete :destroy, :id => 3
+      expect(Folder.find_by_id(@user.default_folder_id)).to be_true
+      delete :destroy, :id => @user.default_folder_id
       expect(response).not_to be_success
       expect(response.code).to eq("409")
-      expect(Folder.find_by_id(3)).to be_true
+      expect(Folder.find_by_id(@user.default_folder_id)).to be_true
     end
     it "moves orphaned conversations for each participant to their default folder" do
       orphanUser = User.create!(:email => 'heehee@example.com',
@@ -74,24 +75,24 @@ describe Api::V0::FoldersController do
                           :password => 'superDUPERsecretPassword')
       orphanUser.default_folder_id = Folder.create!(:name => 'Safety Folder').id
       orphanUser.save
-      expect(User.find_by_id(2).default_folder_id).to eq(3)
-      expect(Folder.find_by_id(3)).to be_true
+      expect(User.find_by_id(orphanUser.id).default_folder_id).to eq(orphanUser.default_folder_id)
+      expect(Folder.find_by_id(orphanUser.default_folder_id)).to be_true
 
       deadFolder = Folder.create!(:name => 'The Walking Dead')
-      expect(Folder.find_by_id(4)).to be_true
+      expect(Folder.find_by_id(deadFolder.id)).to be_true
       conversation = deadFolder.conversations.create(:title => 'Gonna Move')
       conversation.users << orphanUser
-      expect(Conversation.find_by_id(1)).to be_true
-      expect(conversation.folders.find_by_id(4)).to be_true
-      expect(conversation.folders.find_by_id(3)).not_to be_true
-      expect(conversation.users.find_by_id(2)).to be_true
+      expect(Conversation.find_by_id(conversation.id)).to be_true
+      expect(conversation.folders.find_by_id(deadFolder.id)).to be_true
+      expect(conversation.folders.find_by_id(orphanUser.default_folder_id)).not_to be_true
+      expect(conversation.users.find_by_id(orphanUser.id)).to be_true
 
-      delete :destroy, :id => 4
+      delete :destroy, :id => deadFolder.id
       expect(response).to be_success
       expect(response.code).to eq("204")
-      expect(Folder.find_by_id(4)).not_to be_true
-      expect(conversation.folders.find_by_id(4)).not_to be_true
-      expect(conversation.folders.find_by_id(3)).to be_true
+      expect(Folder.find_by_id(deadFolder.id)).not_to be_true
+      expect(conversation.folders.find_by_id(deadFolder.id)).not_to be_true
+      expect(conversation.folders.find_by_id(orphanUser.default_folder_id)).to be_true
     end
   end
 
