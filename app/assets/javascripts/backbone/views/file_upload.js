29731dc3d76cab723fc08a9918ff3b4c48f14c23
William Lubelski <will.lubelski@gmail.com>
2014-02-01 15:07:53 -0500

    style up the file upload popover

--- app/assets/javascripts/backbone/views/file_upload.js
+++ app/assets/javascripts/backbone/views/file_upload.js
@@ -1,4 +1,13 @@
 Structural.Views.FileUpload = Support.CompositeView.extend({
   className: 'act-fileupload',
-  template: JST.template('actions/file_upload')
+  template: JST.template('actions/file_upload'),
+  events: {
+    'click .act-file-upload-toggle': 'toggleFileUpload',
+    'click .act-file-upload-popover .popover-close': 'toggleFileUpload'
+  },
+  toggleFileUpload: function(e){
+    e.preventDefault();
+    this.$('.act-file-upload-popover').toggleClass('hidden');
+    this.$('.act-file-upload-toggle').toggleClass('active');
+  }
 });
