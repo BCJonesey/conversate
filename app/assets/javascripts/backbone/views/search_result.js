ca022d93e1c4d071160e43d4a3c220dee32d798b
Sean Kermes <skermes@gmail.com>
2014-02-03 15:29:40 -0500

    Different classes/templates for cnv/act results.

--- app/assets/javascripts/backbone/views/search_result.js
+++ app/assets/javascripts/backbone/views/search_result.js
@@ -1,12 +1,23 @@
 Structural.Views.SearchResult = Support.CompositeView.extend({
   tagName: 'li',
-  className: 'search-result',
-  template: JST.template('search/result'),
+  className: function() {
+    var classes = 'search-result';
+
+    if (this.model.get('result_type')) {
+      classes += ' ' + this.model.get('result_type') + '-result';
+    }
+
+    return classes;
+  },
+  templates: {
+    'action': JST.template('search/action_result'),
+    'conversation': JST.template('search/conversation_result')
+  },
   initialize: function(options) {
 
   },
   render: function() {
-    this.$el.html(this.template({
+    this.$el.html(this.templates[this.model.get('result_type')]({
       result: this.model
     }));
   }
