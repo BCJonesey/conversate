d996cee4a75a04ed50d9fa63a6ea47693a01e85a
Sean Kermes <skermes@gmail.com>
2014-02-03 16:05:23 -0500

    Split headlines on server, assemble on client.
    This frees us from relying on postgres' headline generator for
    inserting tags, and lets us make sure we're escaping our data
    correctly.
    
    The boundary character we're using is the non-printable ASCII
    record separator character.  If someone tried hard enough they
    could probably get it into our database somehow, but it's more
    than good enough for now.

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -6,7 +6,7 @@
     @result_type = type
     @result_id = result['id']
     @rank = result['rank']
-    @headline = result['headline']
+    @headline = split_headline(result['headline'])
     @conversation_title = result['title']
     @conversation_participants = JSON.load(result['participants'])
   end
@@ -17,7 +17,7 @@
 
     results = conn.exec_query("
       select act.id, ts_rank_cd(search_vector, query, 32) as rank,
-             ts_headline((data->'text')::text, query) as headline,
+             ts_headline((data->'text')::text, query, 'StartSel=\u001E,StopSel=\u001E') as headline,
              cnv.title, cnv.id, json_agg(users.full_name) as participants
       from plainto_tsquery(#{escaped_query}) query, actions as act
       join conversations as cnv
@@ -32,5 +32,20 @@
     ")
 
     results.map {|r| SearchResult.new('action', r) }
+  end
+
+  private
+
+  # Takes a headline annotated by postgres' ts_headline function (with some
+  # particular options) and returns an array of headline fragments.  Expects
+  # ts_headline to have been passed 'StartSel=\u001E,StopSel=\u001E', which
+  # will use the ascii non-printable "record separator" character before and
+  # after each highlighted headline fragment.  By splitting on that, this
+  # returns an array of alternating non-highlighted and highlighted fragments.
+  # When the headline starts with a highlighted fragment (and so the first
+  # character in the string is \u001E) the first element of the array is an
+  # empty string.
+  def split_headline(headline)
+    headline.split("\u001E")
   end
 end
