91c757e5bdf006cf663bd06c905d72c7060accd5
Sean Kermes <skermes@gmail.com>
2014-02-03 13:44:19 -0500

    Inflate SearchResult models.

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,24 +1,24 @@
-class SearchResult
+  class SearchResult
   attr_reader :result_type, :result_id, :rank, :headline,
               :conversation_title, :conversation_participants
 
   def initialize(type, result)
     @result_type = type
-    @result_id = result[0]
-    @rank = result[1]
-    @headline = result[2]
-    @conversation_title = result[3]
-    @conversation_participants = JSON.load(result[4])
+    @result_id = result['id']
+    @rank = result['rank']
+    @headline = result['headline']
+    @conversation_title = result['title']
+    @conversation_participants = JSON.load(result['participants'])
   end
 
   def SearchResult.actions(query)
     conn = ActiveRecord::Base.connection
     escaped_query = conn.quote(query)
 
-    conn.exec_query("
+    results = conn.exec_query("
       select act.id, ts_rank_cd(search_vector, query, 32) as rank,
              ts_headline((data->'text')::text, query) as headline,
-             cnv.title, cnv.id, json_agg(users.full_name)
+             cnv.title, cnv.id, json_agg(users.full_name) as participants
       from plainto_tsquery(#{escaped_query}) query, actions as act
       join conversations as cnv
       on act.conversation_id = cnv.id
@@ -30,5 +30,7 @@ class SearchResult
       group by act.id, rank, headline, cnv.title, cnv.id
       order by rank
     ")
+
+    results.map {|r| SearchResult.new('action', r) }
   end
 end
