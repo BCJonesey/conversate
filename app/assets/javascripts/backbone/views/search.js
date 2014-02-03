be6ad3360755682f480fe09f809b7d97c08120e4
Sean Kermes <skermes@gmail.com>
2014-02-03 13:38:54 -0500

    Open and close search modal.

--- app/assets/javascripts/backbone/views/search.js
+++ app/assets/javascripts/backbone/views/search.js
@@ -1,12 +1,29 @@
 Structural.Views.Search = Support.CompositeView.extend({
   tagName: 'span',
-  className: 'search',
+  className: 'search-wrap popover-wrap',
   template: JST.template('search/search'),
   initialize: function(options) {
-
+    Structural.on('clickAnywhere', this.hide, this);
   },
   render: function() {
     this.$el.html(this.template());
     return this;
+  },
+  events: {
+    'click .search-toggle': 'toggleSearch'
+  },
+
+  toggleSearch: function() {
+    this.$('.search-popover').toggleClass('hidden');
+    this.$('.search-toggle').toggleClass('active');
+  },
+  isOpen: function() {
+    return !this.$('.search-popover').hasClass('hidden');
+  },
+  hide: function(e) {
+    var target = $(e.target);
+    if (target.closest('.search-wrap').length === 0 && this.isOpen()) {
+      this.toggleSearch();
+    }
   }
 });
