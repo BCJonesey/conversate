fdf6d03b91878704c462e2fe0270ee2192187357
Sean Kermes <skermes@gmail.com>
2014-02-03 14:52:13 -0500

    Logical or, not bitwise or.

--- app/assets/javascripts/backbone/views/search_input.js
+++ app/assets/javascripts/backbone/views/search_input.js
@@ -2,7 +2,7 @@ Structural.Views.SearchInput = Support.CompositeView.extend({
   className: 'search-input',
   template: JST.template('search/input'),
   initialize: function(options) {
-
+    this._query = options.query || "";
   },
   render: function() {
     this.$el.html(this.template());
