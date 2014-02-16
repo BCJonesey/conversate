a831333c21c5683652304acc86c4143d140d2cb4
Nick Martin <martin.nick@gmail.com>
2014-02-16 15:28:51 -0500

    Towards a file upload test.

--- spec/controllers/api/v0/actions_controller_spec.rb
+++ spec/controllers/api/v0/actions_controller_spec.rb
@@ -223,6 +223,10 @@ describe Api::V0::ActionsController do
       expect(response).not_to be_success
       expect(response.code).to eq("404")
     end
+    it 'successfully adds a upload action', :t => true do
+      upload_file = fixture_file_upload('/files/upload.png', 'image/png')
+      post :create, :conversation_id => 100, :type => 'upload', :bulkfile => upload_file
+    end
   end
 
 end
