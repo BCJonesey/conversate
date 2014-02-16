0ca7e6f82f3dbfb6eaaacb62431f463e929f88a4
Nick Martin <martin.nick@gmail.com>
2014-02-16 16:43:25 -0500

    Adding the uploads model.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,2 +1,4 @@
 class Upload < ActiveRecord::Base
+  # This method associates the attribute ":upload" with a file attachment
+  has_attached_file :upload
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216213328) do
+ActiveRecord::Schema.define(version: 20140216214154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,10 @@ ActiveRecord::Schema.define(version: 20140216213328) do
   create_table "uploads", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "upload_file_name"
+    t.string   "upload_content_type"
+    t.integer  "upload_file_size"
+    t.datetime "upload_updated_at"
   end
 
   create_table "users", force: true do |t|
