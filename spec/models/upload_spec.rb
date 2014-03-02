d8388e577a6c96ee83d9be671047d4a045c7c1c9
Nick Martin <martin.nick@gmail.com>
2014-03-02 15:11:21 -0500

    Better upload test.

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -5,6 +5,8 @@ describe Upload do
     upload_file = fixture_file_upload('/files/scream.jpeg', 'image/jpeg')
     file = Upload.new(:upload => upload_file)
     file.save
-    expect(file.upload.name).to eq('banzai')
+    expect(file.upload_file_name).to eq('scream.jpeg')
+    expect(file.upload_content_type).to eq('image/jpeg')
+    expect(file.upload_file_size).to eq(9227)
   end
 end
