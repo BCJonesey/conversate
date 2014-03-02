8161cb53c8ff5d654e91bd156bc7bbd53a230aad
Sean Kermes <skermes@gmail.com>
2014-03-02 13:08:48 -0500

    Refactor humanized timestamp into functions.

--- app/assets/javascripts/backbone/support/timestamps.js
+++ app/assets/javascripts/backbone/support/timestamps.js
@@ -7,6 +7,37 @@
 Support.HumanizedTimestamp = function(property) {
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
                 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  var hourMinuteStamp = function(date) {
+    var hours = date.getHours();
+    var meridian = 'am';
+    if (hours >= 12) {
+      meridian = 'pm';
+      hours -= 12;
+    }
+
+    if (hours === 0) {
+      hours = 12;
+    }
+
+    var minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    return hours + ':' + minutes + ' ' + meridian;
+  };
+
+  var monthDateStamp = function(date) {
+    return months[date.getMonth()] + ' ' + date.getDate();
+  };
+
+  var yearMonthDateStamp = function(date) {
+    return (date.getMonth() + 1) + '/' +
+           date.getDate() + '/' +
+           (date.getFullYear() + '').substring(2);
+  };
+
   return {
     humanizedTimestamp: function(relativeTo) {
       relativeTo = relativeTo || new Date();
@@ -15,29 +46,13 @@ Support.HumanizedTimestamp = function(property) {
       if(date.getFullYear() === relativeTo.getFullYear() &&
          date.getMonth() === relativeTo.getMonth() &&
          date.getDate() === relativeTo.getDate()) {
-        var hours = date.getHours();
-        var meridian = 'am';
-        if (hours >= 12) {
-          meridian = 'pm';
-          hours -= 12;
-        }
-
-        if (hours === 0) {
-          hours = 12;
-        }
-
-        var minutes = date.getMinutes();
-        if (minutes < 10) {
-          minutes = '0' + minutes;
-        }
-
-        return hours + ':' + minutes + ' ' + meridian;
+        return hourMinuteStamp(date);
       }
       else if (date.getFullYear() === relativeTo.getFullYear()) {
-        return months[date.getMonth()] + ' ' + date.getDate();
+        return monthDateStamp(date);
       }
       else {
-        return (date.getMonth() + 1) + '/' + date.getDate() + '/' + (date.getFullYear() + '').substring(2);
+        return yearMonthDateStamp(date);
       }
     }
   };
