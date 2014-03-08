c55436c77f82ae0259c103b89772bcfbe49dfb73
Ben Jones <ben@cerberusentertainment.com>
2014-03-08 17:27:51 -0500

    stray comment removal

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@watercooler.io"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.reset_password_email.subject
-  #
   # app/mailers/user_mailer.rb
   def reset_password_email(user)
     @user = user
