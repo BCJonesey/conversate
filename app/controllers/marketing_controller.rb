9080d15e773eeffe206a55c0dd42b0f6d3c50b88
Sean Kermes <skermes@gmail.com>
2014-03-09 13:26:24 -0400

    Remove passcode from marketing page.

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,6 +1,4 @@
 class MarketingController < ApplicationController
-  before_filter :require_speakeasy
-
   def index
   end
 
@@ -8,11 +6,5 @@ class MarketingController < ApplicationController
   end
 
   def pricing
-  end
-
-  private
-
-  def require_speakeasy
-    redirect_to root_url unless session[:code_word] == SpeakeasyCode
   end
 end
