48936fa7a978975830f94dc26abc84835c01cda5
Sean Kermes <skermes@gmail.com>
2014-03-09 14:45:37 -0400

    Don't toast on errors not from http responses.

--- app/assets/javascripts/backbone/monkeypatches/sync_error_toast.js
+++ app/assets/javascripts/backbone/monkeypatches/sync_error_toast.js
@@ -5,10 +5,17 @@
   Backbone.sync = function(method, model, options) {
     var originalError = options.error;
     options.error = function(model, resp, options) {
-      Structural.Toaster.toast({
-        state: 'error',
-        errorType: 'backbone:sync'
-      });
+      // When the response status is 0 we didn't actually get an HTTP response,
+      // the request was killed by something the browser is doing, like
+      // refreshing the page or the computer going to sleep.  Not actually an
+      // error.
+
+      if (model.status >= 300) {
+        Structural.Toaster.toast({
+          state: 'error',
+          errorType: 'backbone:sync'
+        });
+      }
 
       if (originalError) {
         originalError(model, resp, options);
