c2bde9718b098e45227b16eebdd42751ad995b85
Nick Martin <martin.nick@gmail.com>
2014-03-15 14:53:59 -0400

    Quick and dirty file upload with a message through the ui.

--- app/controllers/api/v0/files_controller.rb
+++ app/controllers/api/v0/files_controller.rb
@@ -2,6 +2,19 @@ class Api::V0::FilesController < ApplicationController
   before_filter :require_login_api
 
   def create
+  	upload_file = params[:files][0]
+  	file = Upload.new(:upload => upload_file)
+    file.save
+    url = file.upload.url
+
+	conversation = Conversation.find_by_id(112)
+    action = conversation.actions.new(:type => 'message',
+                                  :data => '{"text":"' + url + '"}',
+                                  :user_id => current_user.id)
+
+    action.save
+    conversation.handle(action)
+
     render :json => {}
   end
 
