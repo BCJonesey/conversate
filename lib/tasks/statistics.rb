5de7f3ee60a0a2cec74a9f978e27a7ae2060d9f1
Sean Kermes <skermes@gmail.com>
2014-03-23 21:13:21 -0400

    Count conversations per user.

--- lib/tasks/statistics.rb
+++ lib/tasks/statistics.rb
@@ -1,15 +1,26 @@
 class Statistics
   def Statistics.run(users)
     all_stats = [
-      :count
+      :count,
+      :count_conversations
     ]
 
     all_stats.each do |stat|
       Statistics.send(stat, users)
+      puts
     end
   end
 
   def Statistics.count(users)
     puts "Total users: #{users.count}"
+  end
+
+  def Statistics.count_conversations(users)
+    convo_counts = users.map{|u| u.conversations.count }.sort
+    median = convo_counts[convo_counts.count / 2]
+
+    puts "Most conversations:   #{convo_counts.last}"
+    puts "Median conversations: #{median}"
+    puts "Fewest conversations: #{convo_counts.first}"
   end
 end
