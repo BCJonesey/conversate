d5cd3f9181b165957737acddb087dc09ca7140e4
Sean Kermes <skermes@gmail.com>
2014-03-23 21:27:33 -0400

    Count conversation length.

--- lib/tasks/statistics.rb
+++ lib/tasks/statistics.rb
@@ -2,7 +2,8 @@ class Statistics
   def Statistics.run(users)
     all_stats = [
       :count,
-      :count_conversations
+      :count_conversations,
+      :conversation_length
     ]
 
     all_stats.each do |stat|
@@ -22,5 +23,18 @@ class Statistics
     puts "Most conversations:   #{convo_counts.last}"
     puts "Median conversations: #{median}"
     puts "Fewest conversations: #{convo_counts.first}"
+  end
+
+  def Statistics.conversation_length(users)
+    action_counts = users.map{|u| u.conversations }
+                         .flatten
+                         .uniq
+                         .map{|c| c.actions.count }
+                         .sort
+    median = action_counts[action_counts.count / 2]
+
+    puts "Longest conversation:  #{action_counts.last}"
+    puts "Median conversation:   #{median}"
+    puts "Shortest conversation: #{action_counts.first}"
   end
 end
