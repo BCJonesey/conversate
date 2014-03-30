892e48595eb1ce7bda9b516625bc3c5e3bfc0f67
Sean Kermes <skermes@gmail.com>
2014-03-30 13:00:51 -0400

    Disabled failing-on-purpose health checks.

--- app/support/health.rb
+++ app/support/health.rb
@@ -28,14 +28,16 @@ module Health
     end
   end
 
-  def Health.user_with_no_groups
-    User.all.keep_if {|u| u.groups.empty? }.map do |u|
-      {
-        :model => u,
-        :notes => ''
-      }
-    end
-  end
+  # Disabled due to all email-created users having no groups by design.
+
+  # def Health.user_with_no_groups
+  #   User.all.keep_if {|u| u.groups.empty? }.map do |u|
+  #     {
+  #       :model => u,
+  #       :notes => ''
+  #     }
+  #   end
+  # end
 
   # Folder health checks
 
@@ -107,24 +109,27 @@ module Health
   # This one is looking for conversations that are in a folder but don't have
   # anyone participating on the conversation that are participating in that
   # folder.
-  def Health.conversation_with_no_participants_in_folder
-    bad_folders = {}
-    Conversation.all.keep_if do |c|
-      c_set = c.users.to_set
-      c.folders.each do |f|
-        f_set = f.users.to_set
-        if c_set.intersection(f_set).empty?
-          bad_folders[c] = bad_folders.fetch(c, []).push(f)
-        end
-      end
-      bad_folders.has_key? c
-    end.map do |c|
-      {
-        :model => c,
-        :notes => bad_folders.fetch(c, []).map{|f| f.debug_s }.join(', ')
-      }
-    end
-  end
+  #
+  # Disabled due to conversations initiated by email not adding users.
+
+  # def Health.conversation_with_no_participants_in_folder
+  #   bad_folders = {}
+  #   Conversation.all.keep_if do |c|
+  #     c_set = c.users.to_set
+  #     c.folders.each do |f|
+  #       f_set = f.users.to_set
+  #       if c_set.intersection(f_set).empty?
+  #         bad_folders[c] = bad_folders.fetch(c, []).push(f)
+  #       end
+  #     end
+  #     bad_folders.has_key? c
+  #   end.map do |c|
+  #     {
+  #       :model => c,
+  #       :notes => bad_folders.fetch(c, []).map{|f| f.debug_s }.join(', ')
+  #     }
+  #   end
+  # end
 
   # Group health checks
 
