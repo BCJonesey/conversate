b9ca7246c961e37f79480d7b398bcefbdb5134e1
Sean Kermes <skermes@gmail.com>
2014-04-26 14:05:07 -0400

    Remove useless comment.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@watercooler.io"
 
-  # app/mailers/user_mailer.rb
   def reset_password_email(user)
     @user = user
     @url  = edit_password_reset_url(user.reset_password_token)
