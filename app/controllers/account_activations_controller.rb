2c25036ce2b4d6042befb99d5c602ad2dbf15c66
Sean Kermes <skermes@gmail.com>
2014-04-30 21:17:21 -0400

    Active accounts after filling out form.

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,5 +6,18 @@ class AccountActivationsController < ApplicationController
   end
 
   def update
+    # TODO: Assert that password and confirmation match
+    @user = User.find_by_activation_token(params[:id])
+    @user.password = params[:password]
+    @user.full_name = params[:name]
+
+    if @user.save
+      @user.activate!
+      login(@user.email, params[:password])
+      redirect_to root_url
+    else
+      flash[:activation_error] = true
+      render :edit
+    end
   end
 end
