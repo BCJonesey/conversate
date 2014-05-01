50f7280dd5b30abc97a7559946230dcdc763d422
Sean Kermes <skermes@gmail.com>
2014-04-30 20:09:45 -0400

    Fill out template for activating account.

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,6 +2,7 @@ class AccountActivationsController < ApplicationController
   skip_before_filter :require_login
 
   def edit
+    @user = User.find_by_activation_token(params[:id])
   end
 
   def update
