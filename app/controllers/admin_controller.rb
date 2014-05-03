3864da92abef3b3f875d6ff666caacd42d21b52e
Sean Kermes <skermes@gmail.com>
2014-05-03 15:57:27 -0400

    Promote button promotes beta users.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,16 @@ class AdminController < ApplicationController
   end
 
   def promote
+    signup = BetaSignup.find(params[:id])
+
+    user = signup.create_promoted_user
+    if user
+      flash[:promotion_status] = :success
+      signup.delete
+    else
+      flash[:promotion_status] = :failure
+    end
+
     redirect_to admin_path
   end
 end
