3f4198e257175b4014e4d7998c0c92a3cfce393a
Nick Martin <nick.martin@tapjoy.com>
2014-06-08 15:21:30 -0400

    Adding a user id column for our invites.

--- db/migrate/20140608185210_create_invites.rb
+++ db/migrate/20140608185210_create_invites.rb
@@ -1,6 +1,7 @@
 class CreateInvites < ActiveRecord::Migration
     def change
         create_table :invites do |t|
+            t.integer :user_id, null: false
             t.string :email, null: false
             t.timestamps
         end
