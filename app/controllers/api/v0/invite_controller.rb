544b3372955343bb8cc292ac95ccd9c0f5579d04
Nick Martin <nick.martin@tapjoy.com>
2014-06-08 16:40:29 -0400

    Making an invite on a post.

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -1,7 +1,9 @@
 class Api::V0::InviteController < ApplicationController
 
   def create
-    invite = Invite.create(params)
+    invite = Invite.create(:email => params[:email], :user_id => params[:user_id])
+    head :status => 500 and return if invite.errors
+    render :json => invite, :status => 201
   end
 
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,5 +1,5 @@
 class Invite < ActiveRecord::Base
-  attr_accessor :email, :user_id
+  attr_accessible :email, :user_id
   validates :email, presence: true, format: /@/
   validates :user_id, presence: true, numericality: {:only_integer => true}
 end
