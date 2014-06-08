ce6d79d0bab8b12319540f954b98f9fa11891bea
Nick Martin <nick.martin@tapjoy.com>
2014-06-08 14:58:33 -0400

    Simple invite model and migration.

---
+++ db/migrate/20140608185210_create_invites.rb
@@ -0,0 +1,8 @@
+class CreateInvites < ActiveRecord::Migration
+    def change
+        create_table :invites do |t|
+            t.string :email, null: false
+            t.timestamps
+        end
+    end
+end
