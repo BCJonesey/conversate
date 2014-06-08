a22052ec12922c5cbee376286adbef6284338d5d
Nick Martin <nick.martin@tapjoy.com>
2014-06-08 16:40:29 -0400

    Making an invite on a post.

--- spec/controllers/api/v0/invite_controller_spec.rb
+++ spec/controllers/api/v0/invite_controller_spec.rb
@@ -5,8 +5,12 @@ describe Api::V0::InviteController do
   context "create" do
 
     it "should be able to make an invite" do
-      post :create
-      binding.pry
+      post :create, :email => 'bob@example.com', :user_id => 1
+      expect(response).to be_success
+      expect(response.code).to eq('201')
+      body = JSON.parse(response.body)
+      expect(body['email']).to eq('bob@example.com')
+      expect(body['user_id']).to eq(1)
     end
 
   end
