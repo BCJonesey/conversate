7e3eb3ac540d47a3908a1701133131d71ceeb1ff
Sean Kermes <skermes@gmail.com>
2014-06-18 15:06:20 -0400

    Uncomment very important home controller code.
    Whoof.  That should never have been committed.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   def index
     if logged_in?
       folder = current_user.folders.first
-      # if folder
-        # redirect_to folder_path(folder.slug, folder.id) and return
-      # else
+      if folder
+        redirect_to folder_path(folder.slug, folder.id) and return
+      else
         @folder = nil
         @folders = []
         @conversation = nil
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
         @participants = nil
 
         render 'structural/show' and return
-      # end
+      end
     end
     render layout: "application_rails"
   end
