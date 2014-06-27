aca6ff077f91b95d4ec6d0038ad64e59a562cd99
Nick Martin <nick.martin@tapjoy.com>
2014-06-26 20:07:36 -0400

    Turning back on validation.s

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -2,7 +2,7 @@ class Api::V0::InviteController < ApplicationController
 
   def create
     invite = Invite.create(:email => params[:email], :user_id => params[:user_id])
-    head :status => 500 and return if false
+    head :status => 500 and return if invite.errors.count > 0
     render :json => invite, :status => 201
 
     UserMailer.send_invite(invite.email).deliver
