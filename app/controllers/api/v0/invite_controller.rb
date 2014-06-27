ac6782369a0b942c9292326616d4cd5b1374e621
Nick Martin <nick.martin@tapjoy.com>
2014-06-26 21:03:27 -0400

    Spec for dealing with the invite counts in a sane manner.

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -2,11 +2,14 @@ class Api::V0::InviteController < ApplicationController
   before_filter :require_login
 
   def create
-    invite = Invite.create(:email => params[:email], :user_id => params[:user_id])
+    invite = Invite.create(:email => params[:email], :user_id => current_user.id)
     head :status => 500 and return if invite.errors.count > 0
     render :json => invite, :status => 201
 
     UserMailer.send_invite(invite.email).deliver
+
+    # If we've successfully sent an email, we want to remove one of the user's invites.
+    current_user.invite_count -= 1
   end
 
 end
--- spec/controllers/api/v0/invite_controller_spec.rb
+++ spec/controllers/api/v0/invite_controller_spec.rb
@@ -5,19 +5,27 @@ describe Api::V0::InviteController do
   before(:each) do
     @user = User.create!(:email => 'dummyUser@example.com',
                           :full_name => 'Rufio Pan',
-                          :password => 'superDUPERsecretPassword')
+                          :password => 'superDUPERsecretPassword',
+                          :invite_count => 10)
     login_user
   end
 
   context "create" do
 
     it "should be able to make an invite" do
-      post :create, :email => 'bob@example.com', :user_id => 1
+      post :create, :email => 'bob@example.com'
       expect(response).to be_success
       expect(response.code).to eq('201')
       body = JSON.parse(response.body)
       expect(body['email']).to eq('bob@example.com')
       expect(body['user_id']).to eq(1)
+    end
+
+    it "should remove one invite from a user on success" do
+      @user.invite_count = 10
+      expect(@user.invite_count).to eq 10
+      post :create, :email => 'bob@example.com'
+      expect(@user.invite_count).to eq 9
     end
 
   end
