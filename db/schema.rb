d6cb1d9cef8f1404bfb44bfcb23f7d7e63e28d37
Nick Martin <nick.martin@tapjoy.com>
2014-06-26 20:49:55 -0400

    Adding a migration for invite count on users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608185210) do
+ActiveRecord::Schema.define(version: 20140627004841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 20140608185210) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer  "default_contact_list_id"
+    t.integer  "invite_count",                    default: 0
   end
 
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
