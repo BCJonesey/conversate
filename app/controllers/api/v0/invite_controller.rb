056b2a29555d487c05334e96c4e8c58ffe457711
Nick Martin <nick.martin@tapjoy.com>
2014-06-29 11:55:11 -0400

    Some mock code for the new code path of invites.

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -1,21 +1,26 @@
 class Api::V0::InviteController < ApplicationController
-  before_filter :require_login
+  #before_filter :require_login
 
   def create
 
     # Users must have invites left.
     head :status => 500 and return unless current_user.invite_count > 0
 
+    # Let's create a new user for this invite.
+    user = User.create!(:email => params[:email])
+
     # Let's create our invite.
+    # TODO: Update invite to refer to our newly created user.
     invite = Invite.create(:email => params[:email], :user_id => current_user.id)
     head :status => 500 and return if invite.errors.count > 0
 
     # Let's try to send the email.
-    begin
-      UserMailer.send_invite(invite.email).deliver
-    rescue Exception
-      head :status => 500 and return
-    end
+    # TODO: Remove, we're letting sorcery handle it.
+    # begin
+    #   UserMailer.send_invite(invite.email).deliver
+    # rescue Exception
+    #   head :status => 500 and return
+    # end
 
     # If we've successfully sent an email, we want to remove one of the user's invites.
     current_user.invite_count -= 1
