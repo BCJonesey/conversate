beec8372ab8e83e3686f866e142a279083534763
Sean Kermes <skermes@gmail.com>
2014-06-29 13:46:25 -0400

    Remove duplicate condition.

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -6,10 +6,12 @@ class FoldersController < ApplicationController
 
     @folder = folder
     @folders = current_user.folders
-    @conversations = folder.conversations.joins(:reading_logs).where('reading_logs.user_id' =>
-      current_user.id).order('most_recent_event DESC')
-    @conversation = @conversations.where('reading_logs.user_id' => current_user.id,
-                                          'reading_logs.archived' => false)[0]
+    @conversations = folder.conversations
+                           .joins(:reading_logs)
+                           .where('reading_logs.user_id' => current_user.id)
+                           .order('most_recent_event DESC')
+    @conversation = @conversations
+                      .where('reading_logs.archived' => false)[0]
     @actions = @conversation ? @conversation.actions : nil
     @participants = @conversation ? @conversation.participants : nil
 
