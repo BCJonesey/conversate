5d8a8734fc4c35297347ed40286d78a488fae8e3
Nick Martin <nick.martin@tapjoy.com>
2014-06-29 11:19:10 -0400

    Moving the render block.

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -9,7 +9,6 @@ class Api::V0::InviteController < ApplicationController
     # Let's create our invite.
     invite = Invite.create(:email => params[:email], :user_id => current_user.id)
     head :status => 500 and return if invite.errors.count > 0
-    render :json => invite, :status => 201
 
     # Let's try to send the email.
     begin
@@ -20,6 +19,8 @@ class Api::V0::InviteController < ApplicationController
 
     # If we've successfully sent an email, we want to remove one of the user's invites.
     current_user.invite_count -= 1
+
+    render :json => invite, :status => 201
   end
 
 end
