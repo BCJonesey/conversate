fef3fd3245ccbfdb7df130b4b0e4a607a94f3495
Nick Martin <nick.martin@tapjoy.com>
2014-06-29 14:21:30 -0400

    Cleaning up the invite controller to use the sorcery style, adding a template for an invite.

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -1,30 +1,20 @@
 class Api::V0::InviteController < ApplicationController
-  #before_filter :require_login
+  before_filter :require_login
 
   def create
 
     # Users must have invites left.
-    #head :status => 500 and return unless current_user.invite_count > 0
+    head :status => 500 and return unless current_user.invite_count > 0
 
     # Let's create a new user for this invite.
-    # TODO: Make sure email is unique.
     user = User.create!(:email => params[:email], :password => "test")
 
     # Let's create our invite.
-    # TODO: Update invite to refer to our newly created user.
     invite = Invite.create(:email => params[:email], :user_id => current_user.id)
     head :status => 500 and return if invite.errors.count > 0
 
-    # Let's try to send the email.
-    # TODO: Remove, we're letting sorcery handle it.
-    # begin
-    #   UserMailer.send_invite(invite.email).deliver
-    # rescue Exception
-    #   head :status => 500 and return
-    # end
-
     # If we've successfully sent an email, we want to remove one of the user's invites.
-    #current_user.invite_count -= 1
+    current_user.invite_count -= 1
 
     render :json => invite, :status => 201
   end
