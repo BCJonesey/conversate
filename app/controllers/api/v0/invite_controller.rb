6b12e4b19cc31f13b4925a0ba17b39e4d99777fd
Ben Jones <ben@cerberusentertainment.com>
2014-08-10 13:57:14 -0400

    invites rejigger

--- app/controllers/api/v0/invite_controller.rb
+++ app/controllers/api/v0/invite_controller.rb
@@ -3,20 +3,9 @@ class Api::V0::InviteController < ApplicationController
 
   def create
 
-    # Users must have invites left.
-    head :status => 500 and return unless current_user.invite_count > 0
+    invite = Invite.build({inviter: current_user, invitee_email: params[:email]})
 
-    # Let's create a new user for this invite.
-    user = User.build(:email => params[:email], :password => SecureRandom.uuid, :creation_source => :invite)
-
-    # Let's create our invite.
-    invite = Invite.create(:email => params[:email], :user_id => user.id, current_user.id)
-    head :status => 500 and return if invite.errors.count > 0
-
-    # If we've successfully sent an email, we want to remove one of the user's invites.
-    current_user.invite_count -= 1
-
-    UserMailer.activation_needed_email(user).deliver
+    render :json => invite, :status => 500 and return if invite.errors.count > 0
 
     render :json => invite, :status => 201
   end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,57 @@
 class Invite < ActiveRecord::Base
   attr_accessible :email, :user_id, :inviter_id
-  validates :email, presence: true, format: /@/
-  validates :user_id, presence: true, numericality: {:only_integer => true}
+  validates :user_id, presence: true, numericality: {:only_integer => true}, , uniqueness: { scope: :inviter_id, message: "A user can only invite a specfic person once" }
   validates :inviter_id, presence: true, numericality: {:only_integer => true}
+
+
+  # Builds an invite.
+  # Params:
+  # inviter: User object that is creating and invitation
+  # invitee_email: The email addy of the person getting invited.
+  def self.build(params)
+    invite = Invite.new()
+
+    inviter = params[:inviter]
+
+    # Users must have invites left.
+    invite.errors.add(:inviter, "Inviter does not have any invites left") and return invite unless inviter.invite_count > 0
+
+    invite.inviter_id = inviter.id
+
+    invitee_email = params[:invitee_email]
+
+    # Find the invited user if they exist
+    user = User.find_by_email(invitee_email)
+
+    # Let's create a new user for this invite if we need to.
+    user = User.build(:email => invitee_email, :password => SecureRandom.uuid, :creation_source => :invite) if user.nil?
+
+    invite.errors.add(:user, "User could not be found or created")  and return invite if user.errors.count > 0
+
+    invite.user = user
+
+    begin
+      UserMailer.activation_needed_email(user).deliver
+    rescue
+      invite.errors.add(:user, "Could not send invitation email") and return invite
+    end
+
+    return invite unless invite.save!
+
+    # Once we secessfully added an invitation, add the inviter to the invitee's contact list, and vice-versa
+    contact = user.default_contact_list.contacts.build()
+    contact.user = inviter
+    contact.save
+
+    contact = inviter.default_contact_list.contacts.build()
+    contact.user = user
+    contact.save
+
+    # If we've successfully sent an email, we want to remove one of the user's invites.
+    inviter.invite_count -= 1
+    inviter.save
+
+    invite
+  end
+
 end
