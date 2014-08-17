b42439c40bb22b1fbd8f08b1d329a725045c8818
Sean Kermes <skermes@gmail.com>
2014-08-17 11:58:02 -0400

    Don't try to render nonexistent template.

--- app/controllers/api/v0/contacts_controller.rb
+++ app/controllers/api/v0/contacts_controller.rb
@@ -22,7 +22,7 @@ class Api::V0::ContactsController < ApplicationController
     if contact.save
       render :json => contact.to_json, :status => 201
     else
-      render :status => 500
+      head :status => 500 and return
     end
   end
 end
