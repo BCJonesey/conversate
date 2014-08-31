e1307e4bb51eae54419e02b4f0c28baedca68900
Sean Kermes <skermes@gmail.com>
2014-08-31 16:21:20 -0400

    Give invited users a contact to WC Support.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -26,7 +26,14 @@ class Invite < ActiveRecord::Base
     user = User.find_by_email(invitee_email)
 
     # Let's create a new user for this invite if we need to.
-    user = User.build(:email => invitee_email, :password => SecureRandom.uuid, :creation_source => :invite) if user.nil?
+    if user.nil?
+      user = User.build(:email => invitee_email,
+                        :password => SecureRandom.uuid,
+                        :creation_source => :invite)
+      support_contact = user.default_contact_list.contacts.build
+      support_contact.user_id = User.support_user_id
+      support_contact.save
+    end
 
     invite.errors.add(:user, "User could not be found or created")  and return invite unless user
 
