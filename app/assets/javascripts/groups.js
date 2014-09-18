1d6bfbf5efb56e6bef5ec5890ea2ee552b2d229d
Sean Kermes <skermes@gmail.com>
2014-09-18 15:05:12 -0400

    Delete groups.js.
    This page doesn't exist anymore.

--- app/assets/javascripts/groups.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('.grp-new-user-trigger').on('click', function(e) {
-    e.preventDefault();
-    var group = $(e.target).parents('.grp').first();
-    group.find('.grp-new-user-form').removeClass('hidden');
-  });
-});
