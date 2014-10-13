109d6af2e7b5c42fa82ae649b668a7b055c64c80
Sean Kermes <skermes@gmail.com>
2014-10-13 17:55:21 -0400

    Build and render devices.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,16 @@
 class Device < ActiveRecord::Base
   belongs_to :user
+
+  def self.new_device_for(user)
+    d = Device.new
+    d.user_id = user.id
+    d.device_api_key = SecureRandom.uuid
+    d.save
+
+    d
+  end
+
+  def as_json(options={})
+    super(:only => [:user_id, :device_api_key])
+  end
 end
